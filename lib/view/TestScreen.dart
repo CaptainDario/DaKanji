40dfe3fa57dfd0b2bda817f5ff39d60f0c9ee860
dario <dario.klepoch@freenet.de>
2022-02-16 16:05:34 +0100

    emptied

--- lib/view/TestScreen.dart
+++ lib/view/TestScreen.dart
@@ -1,6 +1,5 @@
-import 'package:da_kanji_mobile/view/FoldingWidget.dart';
-import 'package:da_kanji_mobile/view/drawing/DrawScreen.dart';
 import 'package:flutter/material.dart';
+
 
 
 class TestScreen extends StatefulWidget {
@@ -12,9 +11,6 @@ class TestScreen extends StatefulWidget {
 
 class _TestScreenState extends State<TestScreen> {
 
-  GlobalKey<FoldingWidgetState> foldingKey = 
-    new GlobalKey<FoldingWidgetState>();
-
   @override
   void initState() {
     super.initState();
@@ -22,22 +18,10 @@ class _TestScreenState extends State<TestScreen> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(title: Text("TestScreen"),),
-      body: Row(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width/2,
-            height: MediaQuery.of(context).size.height,
-            child: DrawScreen(false, false)
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width/2,
-            height: MediaQuery.of(context).size.height,
-            child: DrawScreen(false, false)
-          )
-        ],
-      ) 
+      body: Center(child: Text("test"))
     );
   }
 }
