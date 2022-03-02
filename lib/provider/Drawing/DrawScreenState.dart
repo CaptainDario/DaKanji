73d7808dbc5da4acb5873f23ff9f9a2444b60f19
dario <dario.klepoch@freenet.de>
2022-03-02 21:19:52 +0100

    added docs

--- lib/provider/Drawing/DrawScreenState.dart
+++ lib/provider/Drawing/DrawScreenState.dart
@@ -10,13 +10,13 @@ class DrawScreenState {
 
   /// the strokes which are drawn on the Canvas of the Drawing Screen
   Strokes strokes;
-
+  /// stores all characters which are in the multi character lookup
   KanjiBuffer kanjiBuffer;
-
+  /// change notifier for looking up characters in dictionaries
   DrawingLookup drawingLookup;
-
+  /// in which layout is the app currently being rendered
   DrawScreenLayout drawScreenLayout;
-
+  /// the current size of the drawing canvas
   double canvasSize = 0.0;
 
   DrawScreenState(
