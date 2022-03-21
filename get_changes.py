6963bce288fc558cf0130ce7c1f72208a85083bd
Dariyooo <dario.klepoch@gmail.com>
2022-03-21 22:41:07 +0100

    added usage note and enacpuslated

--- get_changes.py
+++ get_changes.py
@@ -1,8 +1,18 @@
+#############################################################
+#                                                           #
+# Script to read the changes of the latest release from the #
+# CHANGELOG.md and print them to std out.                   #
+#                                                           #
+#############################################################
+
+
+
 import re
 
 
-with open("CHANGELOG.md", mode="r", encoding="utf8") as f:
-    changelog = f.read()
+if __name__ == "__main__":
+    with open("CHANGELOG.md", mode="r", encoding="utf8") as f:
+        changelog = f.read()
 
-newest_changes = re.search("(##.*?)----------------", changelog, re.DOTALL).groups()[0]
-print(newest_changes)
+    newest_changes = re.search("(##.*?)----------------", changelog, re.DOTALL).groups()[0]
+    print(newest_changes)
