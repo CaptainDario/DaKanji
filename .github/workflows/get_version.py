bff785b7d4271df61ceeb2592b88067546262a3e
Dariyooo <dario.klepoch@gmail.com>
2022-03-22 21:05:49 +0100

    updated workflows

---
+++ .github/workflows/get_version.py
@@ -0,0 +1,23 @@
+#############################################################
+#                                                           #
+# Script to read the version of the latest release from the #
+# pubspec.yaml and dump them to current_version.txt.        #
+#                                                           #
+#############################################################
+
+
+
+import re
+
+
+
+if __name__ == "__main__":
+
+
+
+    with open("pubspec.yaml") as f:
+
+        version = re.search("version: (.*)", f.read()).groups()[0]
+        
+        with open("dakanji_version.txt", mode="w+", encoding="utf8") as f:
+            f.write("DAKANJI_VERSION=" + version)
