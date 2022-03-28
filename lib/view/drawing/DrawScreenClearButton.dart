00d0e81be99cd5c955e83e84a3d05cf256f105ab
Dariyooo <dario.klepoch@gmail.com>
2022-03-28 02:34:07 +0200

    refactored clearbutton into separate widget

---
+++ lib/view/drawing/DrawScreenClearButton.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+import 'package:feature_discovery/feature_discovery.dart';
+
+import 'package:da_kanji_mobile/provider/drawing/Strokes.dart';
+
+
+
+class DrawScreenClearButton extends StatelessWidget {
+  const DrawScreenClearButton(
+    this.canvasSize,
+    {Key? key}
+  ) : super(key: key);
+
+  final double canvasSize;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Strokes>(
+      builder: (contxt, strokes, _) {
+        return DescribedFeatureOverlay(
+          tapTarget: Icon(Icons.add),
+          featureId: "draw_screen_01",
+          backgroundColor: Color.fromARGB(0, 0, 0, 0),
+          backgroundOpacity: 0,
+          targetColor: Color.fromARGB(148, 255, 255, 255),
+          title: Text("hello"),
+          child: Center(
+            child: Container(
+              width: canvasSize * 0.1,
+              child: FittedBox(
+                child: IconButton(
+                  icon: Icon(Icons.clear),
+                  iconSize: 100,
+                  color: Theme.of(context).highlightColor,
+                  onPressed: () {
+                    strokes.playDeleteAllStrokesAnimation = true;
+                  }
+                ),
+              ),
+            ),
+          ),
+        );
+      }
+    );
+  }
+}
