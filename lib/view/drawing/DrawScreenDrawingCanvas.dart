29b80c5c0eb29075195028c7a2caf8d86d06ad78
Dariyooo <dario.klepoch@gmail.com>
2022-03-28 02:34:44 +0200

    refactored DrawingCanvas into separate widget

---
+++ lib/view/drawing/DrawScreenDrawingCanvas.dart
@@ -0,0 +1,46 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import 'package:da_kanji_mobile/model/core/DrawingInterpreter.dart';
+import 'package:da_kanji_mobile/provider/drawing/Strokes.dart';
+import 'package:da_kanji_mobile/view/drawing/DrawingCanvas.dart';
+
+
+
+class DrawScreenDrawingCanvas extends StatelessWidget {
+  const DrawScreenDrawingCanvas(
+    this.canvasSize,
+    this.drawingInterpreter,
+    {Key? key}
+  ) : super(key: key);
+
+  final double canvasSize;
+  final DrawingInterpreter drawingInterpreter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Strokes>(
+      builder: (context, strokes, __){
+        return DrawingCanvas(
+          canvasSize, canvasSize,
+          strokes,
+          EdgeInsets.all(0),
+          onFinishedDrawing: (Uint8List image) async {
+            drawingInterpreter.runInference(image);
+          },
+          onDeletedLastStroke: (Uint8List image) {
+            if(strokes.strokeCount > 0)
+              drawingInterpreter.runInference(image);
+            else
+              drawingInterpreter.clearPredictions();
+          },
+          onDeletedAllStrokes: () {
+            drawingInterpreter.clearPredictions();
+          },
+        );
+      },
+    );
+  }
+}
