ddfac7f7d2dcfff11a90046fdfae669a5a16e70f
CaptainDario - Ubuntu <daapplab@gmail.com>
2022-05-02 20:36:59 +0200

    merge with main

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,14 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/dario/dev/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/dario/dev/DaKanji-Application" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/dario/dev/flutter"
+  "PROJECT_DIR=/home/dario/dev/DaKanji-Application"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=/home/dario/dev/DaKanji-Application/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=lib/main.dart"
+)
