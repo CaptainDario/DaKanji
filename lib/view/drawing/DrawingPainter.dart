064d55d8f3d294ecd2f3a0cd0ab0e8f5e857ab70
CaptainDario <51273483+CaptainDario@users.noreply.github.com>
2022-07-16 22:59:05 +0200

    use same scaling for both axis

--- lib/view/drawing/DrawingPainter.dart
+++ lib/view/drawing/DrawingPainter.dart
@@ -119,8 +119,10 @@ class DrawingPainter extends CustomPainter {
           .shift(Offset(-b.center.dx, -b.center.dy))
           // scale to 85% of canvas
           .transform(transformationMatrix(
-            scaleX: _size.width / (b.height > b.width ? b.height : b.width) * 0.75,
-            scaleY: _size.height / (b.height > b.width ? b.height : b.width) * 0.75
+            //scaleX: _size.width / b.width * 0.75,
+            //scaleY: _size.height / b.height * 0.75
+            scaleX: _size.width / (b.height > b.width ? b.height : b.width) * 0.8,
+            scaleY: _size.height / (b.height > b.width ? b.height : b.width) * 0.8
           ))
           // move to center
           .shift(Offset(_size.width / 2.0, _size.height / 2.0)),
