7f8d1647f9da2fe8500a1451b1592c9bc8692e14
CaptainDario <51273483+CaptainDario@users.noreply.github.com>
2022-07-16 11:52:53 +0200

    expose CoreML delegate

--- plugins/tflite_flutter_plugin/lib/tflite_flutter.dart
+++ plugins/tflite_flutter_plugin/lib/tflite_flutter.dart
@@ -14,6 +14,7 @@ export 'src/delegate.dart';
 export 'src/delegates/gpu_delegate.dart';
 export 'src/delegates/metal_delegate.dart';
 export 'src/delegates/xnnpack_delegate.dart';
+export 'src/delegates/coreml_delegate.dart';
 export 'src/interpreter.dart';
 export 'src/interpreter_options.dart';
 export 'src/quanitzation_params.dart';
