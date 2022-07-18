bb14490d2ea2b63c52fc66754257609fdb4c6c8f
CaptainDario <51273483+CaptainDario@users.noreply.github.com>
2022-07-18 22:47:53 +0200

    generated stuff

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,8 @@ import Foundation
 
 import device_info_plus_macos
 import in_app_review
+import mecab_dart
+import path_provider_macos
 import shared_preferences_macos
 import url_launcher_macos
 import window_size
@@ -14,6 +16,8 @@ import window_size
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
+  MecabDartPlugin.register(with: registry.registrar(forPlugin: "MecabDartPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  mecab_dart
   url_launcher_windows
   window_size
 )
