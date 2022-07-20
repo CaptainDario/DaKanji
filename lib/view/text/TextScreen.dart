bbbee28581de88c27b7498446198f3d57c7eba36
CaptainDario <51273483+CaptainDario@users.noreply.github.com>
2022-07-20 12:52:42 +0200

    added: animations, styling, paste button. functionality to buttons

--- lib/view/text/TextScreen.dart
+++ lib/view/text/TextScreen.dart
@@ -1,14 +1,15 @@
 
 import 'package:da_kanji_mobile/view/text/TextWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 import 'package:kagome_dart/kagome_dart.dart';
 import 'package:tuple/tuple.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:da_kanji_mobile/model/Screens.dart';
-import 'package:da_kanji_mobile/model/DrawScreen/DrawingInterpreter.dart';
 import 'package:da_kanji_mobile/model/DrawScreen/DrawScreenState.dart';
 import 'package:da_kanji_mobile/view/drawer/Drawer.dart';
 
@@ -42,15 +43,21 @@ class _TextScreenState extends State<TextScreen> with TickerProviderStateMixin {
 
   final double padding = 8.0;
 
+  bool fullScreen = false;
+
+  bool showFurigana = false;
+
 
   @override
   void initState() {
     super.initState();
 
+    initTokenizer();
+
     // initialize the drawing interpreter if it has not been already
-    if(!GetIt.I<DrawingInterpreter>().wasInitialized){
-      GetIt.I<DrawingInterpreter>().init();
-    }
+    //if(!GetIt.I<DrawingInterpreter>().wasInitialized){
+    //  GetIt.I<DrawingInterpreter>().init();
+    //}
 
     /*WidgetsBinding.instance.addPostFrameCallback((Duration timeStamp) {
       final OnboardingState? onboarding = Onboarding.of(context);
@@ -83,45 +90,154 @@ class _TextScreenState extends State<TextScreen> with TickerProviderStateMixin {
         child: LayoutBuilder(
           builder: (context, constraints){
               
-            return Padding(
-              padding: EdgeInsets.all(padding),
-              child: Column(
-                children: [
-                  Card(
-                    child: Container(
-                      width: constraints.maxWidth,
-                      height: constraints.maxHeight/2-2*padding,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextField(
-                          decoration: new InputDecoration(
-                            hintText: "Input text here..."
+            return Container(
+              height: constraints.maxHeight,
+              width: constraints.maxWidth,
+              child: Padding(
+                padding: EdgeInsets.all(padding),
+                child: Stack(
+                  children: [
+                    // Text input
+                    Card(
+                      child: Container(
+                        width: constraints.maxWidth,
+                        height: constraints.maxHeight/2-2*padding,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              TextField(
+                                decoration: new InputDecoration(
+                                  hintText: "Input text here..."
+                                ),
+                                controller: widget.inputController,
+                                maxLines: null,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                                onChanged: ((value) {
+                                  setState(() {
+                                    analyzed = runAnalyzer(value, AnalyzeModes.normal);
+                                  });
+                                }),
+                              ),
+                              SizedBox(height: padding*4,),
+                              Visibility(
+                                visible: widget.inputController.text.length == 0,
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey,
+                                      borderRadius: BorderRadius.all(Radius.circular(20))
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(4.0),
+                                      child: TextButton.icon(
+                                        label: Text("Paste"),
+                                        icon: Icon(Icons.paste),
+                                        onPressed: () async {
+                                          ClipboardData? d = await Clipboard.getData("text/plain");
+
+                                          if(d != null) 
+                                              widget.inputController.text = d.text!;
+                                          else
+                                            widget.inputController.text = "";
+
+                                          setState(() {
+                                            
+                                          });
+                                        }
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
                           ),
-                          controller: widget.inputController,
-                          maxLines: null,
-                          style: TextStyle(
-                            fontSize: 20,
-                          ),
-                          onChanged: ((value) {
-                            analyzed = runAnalyzer(value, AnalyzeModes.search);
-                            print(analyzed);
-                            setState(() {
-                              
-                            });
-                          }),
-                        ),
-                      )
+                        )
+                      ),
                     ),
-                  ),
-                  TextWidget(
-                    texts: analyzed.item1,
-                    rubys: analyzed.item2.map(
-                      (e) => (e.length == 9 ? e[7] : "")
-                    ).toList(),
-                    width: constraints.maxWidth,
-                    height: constraints.maxHeight/2-2*padding,
-                  )
-                ]
+                    // processed
+                    Positioned(
+                      bottom: 0,
+                      child: Card(
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 500),
+                          width: constraints.maxWidth-3*padding,
+                          height: !fullScreen ? 
+                            constraints.maxHeight/2-2*padding : 
+                            constraints.maxHeight - 3*padding,
+                          child: Padding(
+                            padding: EdgeInsets.all(padding),
+                            child: Stack(
+                              children: [
+                                Positioned(
+                                  child: AnimatedContainer(
+                                    width: constraints.maxWidth,
+                                    height: !fullScreen ? 
+                                      constraints.maxHeight/2-2*padding - 
+                                      (50 + 2*padding) :
+                                      constraints.maxHeight - 2*padding,
+                                    duration: Duration(milliseconds: 500),
+                                    child: TextWidget(
+                                      texts: analyzed.item1,
+                                      rubys: analyzed.item2.map(
+                                        (e) => (e.length == 9 ? e[7] : "")
+                                      ).toList(),
+                                      
+                                      showFurigana: showFurigana,
+                                    ),
+                                  ),
+                                ),
+                                Positioned(
+                                  height: 1,
+                                  width: constraints.maxWidth,
+                                  bottom: 25 + 24,
+                                  child: Divider(),
+                                ),
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  height: 40,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      IconButton(
+                                        padding: EdgeInsets.zero,
+                                        onPressed: () => setState(() {
+                                          showFurigana = !showFurigana;
+                                        }), 
+                                        icon: SvgPicture.asset(
+                                          showFurigana ?
+                                          "assets/images/ui/furigana_on.svg" : 
+                                          "assets/images/ui/furigana_off.svg",
+                                          color: Colors.white,
+                                          height: 20,
+                                        )
+                                      ),
+                                      IconButton(
+                                        iconSize: 25,
+                                        padding: EdgeInsets.zero,
+                                        onPressed: () => setState(() {
+                                          fullScreen = !fullScreen;
+                                        }), 
+                                        icon: Icon(!fullScreen ? 
+                                          Icons.fullscreen : 
+                                          Icons.fullscreen_exit
+                                        )
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ]
+                ),
               ),
             );
           }
