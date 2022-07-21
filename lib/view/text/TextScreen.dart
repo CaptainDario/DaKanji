c2e50b94135b592e5f7b27f630abd9b6e65f6c4a
CaptainDario <51273483+CaptainDario@users.noreply.github.com>
2022-07-21 12:50:18 +0200

    fixed: ui issues and small redesign

--- lib/view/text/TextScreen.dart
+++ lib/view/text/TextScreen.dart
@@ -47,12 +47,26 @@ class _TextScreenState extends State<TextScreen> with TickerProviderStateMixin {
 
   bool showFurigana = false;
 
+  bool showSpaces = false;
+
+
+  late final AnimationController _controller;
+  late Animation _animation;
+
 
   @override
   void initState() {
+
     super.initState();
 
     initTokenizer();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 500),
+      value: 0.0,
+      vsync: this,
+    );
+
 
     // initialize the drawing interpreter if it has not been already
     //if(!GetIt.I<DrawingInterpreter>().wasInitialized){
@@ -74,12 +88,20 @@ class _TextScreenState extends State<TextScreen> with TickerProviderStateMixin {
 
   @override
   void dispose() {
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
 
+    _animation = new Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(new CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut
+    ));
     
 
     return DaKanjiDrawer(
@@ -90,155 +112,149 @@ class _TextScreenState extends State<TextScreen> with TickerProviderStateMixin {
         child: LayoutBuilder(
           builder: (context, constraints){
               
-            return Container(
-              height: constraints.maxHeight,
-              width: constraints.maxWidth,
-              child: Padding(
-                padding: EdgeInsets.all(padding),
-                child: Stack(
-                  children: [
-                    // Text input
-                    Card(
-                      child: Container(
-                        width: constraints.maxWidth,
-                        height: constraints.maxHeight/2-2*padding,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            children: [
-                              TextField(
-                                decoration: new InputDecoration(
-                                  hintText: "Input text here..."
-                                ),
-                                controller: widget.inputController,
-                                maxLines: null,
-                                style: TextStyle(
-                                  fontSize: 20,
-                                ),
-                                onChanged: ((value) {
-                                  setState(() {
-                                    analyzed = runAnalyzer(value, AnalyzeModes.normal);
-                                  });
-                                }),
-                              ),
-                              SizedBox(height: padding*4,),
-                              Visibility(
-                                visible: widget.inputController.text.length == 0,
-                                child: Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                      color: Colors.grey,
-                                      borderRadius: BorderRadius.all(Radius.circular(20))
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(4.0),
-                                      child: TextButton.icon(
-                                        label: Text("Paste"),
-                                        icon: Icon(Icons.paste),
-                                        onPressed: () async {
-                                          ClipboardData? d = await Clipboard.getData("text/plain");
-
-                                          if(d != null) 
-                                              widget.inputController.text = d.text!;
-                                          else
-                                            widget.inputController.text = "";
-
-                                          setState(() {
-                                            
-                                          });
-                                        }
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        )
-                      ),
-                    ),
-                    // processed
-                    Positioned(
-                      bottom: 0,
-                      child: Card(
-                        child: AnimatedContainer(
-                          duration: const Duration(milliseconds: 500),
-                          width: constraints.maxWidth-3*padding,
-                          height: !fullScreen ? 
-                            constraints.maxHeight/2-2*padding : 
-                            constraints.maxHeight - 3*padding,
+            return AnimatedBuilder(
+              animation: _controller,
+              builder: (context, builder) {
+                return Container(
+                  height: constraints.maxHeight,
+                  width: constraints.maxWidth,
+                  child: Padding(
+                    padding: EdgeInsets.all(padding),
+                    child: Stack(
+                      children: [
+                        // Text input
+                        Container(
+                          width: constraints.maxWidth,
+                          height: constraints.maxHeight/2-2*padding,
                           child: Padding(
-                            padding: EdgeInsets.all(padding),
-                            child: Stack(
-                              children: [
-                                Positioned(
-                                  child: AnimatedContainer(
-                                    width: constraints.maxWidth,
-                                    height: !fullScreen ? 
-                                      constraints.maxHeight/2-2*padding - 
-                                      (50 + 2*padding) :
-                                      constraints.maxHeight - 2*padding,
-                                    duration: Duration(milliseconds: 500),
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextField(
+                              decoration: new InputDecoration(
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              enabledBorder: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                              disabledBorder: InputBorder.none,
+                                hintText: "Input text here..."
+                              ),
+                              controller: widget.inputController,
+                              maxLines: null,
+                              style: TextStyle(
+                                fontSize: 20,
+                              ),
+                              onChanged: ((value) {
+                                setState(() {
+                                  analyzed = runAnalyzer(value, AnalyzeModes.normal);
+                                });
+                              }),
+                            ),
+                          )
+                        ),
+                        // processed
+                        Positioned(
+                          bottom: 0,
+                          child: Container(
+                            width: constraints.maxWidth-3*padding,
+                            height: (constraints.maxHeight/2-(fullScreen ? 1.5 : 2)*padding) 
+                                * (_animation.value+1.0),
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).canvasColor,
+                              borderRadius: BorderRadius.circular(5.0)
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.all(padding),
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                    child: Divider(
+                                      color: Theme.of(context).hintColor,
+                                    ),
+                                    width: 50,
+                                    height: 1,
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Expanded(
                                     child: TextWidget(
                                       texts: analyzed.item1,
                                       rubys: analyzed.item2.map(
                                         (e) => (e.length == 9 ? e[7] : "")
                                       ).toList(),
-                                      
                                       showFurigana: showFurigana,
+                                      addSpaces: showSpaces,
                                     ),
                                   ),
-                                ),
-                                Positioned(
-                                  height: 1,
-                                  width: constraints.maxWidth,
-                                  bottom: 25 + 24,
-                                  child: Divider(),
-                                ),
-                                Positioned(
-                                  bottom: 0,
-                                  right: 0,
-                                  height: 40,
-                                  child: Row(
+                                  Divider(),
+                                  Row(
                                     mainAxisAlignment: MainAxisAlignment.end,
                                     children: [
-                                      IconButton(
-                                        padding: EdgeInsets.zero,
-                                        onPressed: () => setState(() {
-                                          showFurigana = !showFurigana;
-                                        }), 
-                                        icon: SvgPicture.asset(
-                                          showFurigana ?
-                                          "assets/images/ui/furigana_on.svg" : 
-                                          "assets/images/ui/furigana_off.svg",
-                                          color: Colors.white,
-                                          height: 20,
-                                        )
+                                      Material(
+                                        child: IconButton(
+                                          icon: SvgPicture.asset(
+                                            !showSpaces ?
+                                            "assets/fonts/icons/space_bar_off.svg" :
+                                            "assets/fonts/icons/space_bar_on.svg",
+                                            color: Colors.white,
+                                          ),
+                                          onPressed: () {
+                                            setState(() {
+                                              showSpaces = !showSpaces;
+                                            });
+                                          },
+                                        ),
                                       ),
-                                      IconButton(
-                                        iconSize: 25,
-                                        padding: EdgeInsets.zero,
-                                        onPressed: () => setState(() {
-                                          fullScreen = !fullScreen;
-                                        }), 
-                                        icon: Icon(!fullScreen ? 
-                                          Icons.fullscreen : 
-                                          Icons.fullscreen_exit
-                                        )
+                                      Material(
+                                        child: IconButton(
+                                          key: GlobalKey(),
+                                          icon: SvgPicture.asset(
+                                            showFurigana ?
+                                            "assets/fonts/icons/furigana_off.svg" :
+                                            "assets/fonts/icons/furigana_on.svg",
+                                            color: Colors.white,
+                                          ),
+                                          onPressed: () {
+                                            setState(() {
+                                              showFurigana = !showFurigana;
+                                            });
+                                          },
+                                        ),
+                                      ),
+                                      Material(
+                                        child: IconButton(
+                                          icon: Icon(!fullScreen ? 
+                                            Icons.fullscreen : 
+                                            Icons.fullscreen_exit
+                                          ),
+                                          onPressed: () {
+                                            // do not allow change while animation is running
+                                            if (_controller.isAnimating)
+                                              return;
+                                      
+                                            if(_controller.isCompleted)
+                                              _controller.reverse();
+                                            else if(_controller.isDismissed)
+                                              _controller.forward();
+
+                                            setState(() {
+                                              fullScreen = !fullScreen;
+                                            });
+                                            
+                                          },
+                                        ),
                                       ),
                                     ],
-                                  ),
-                                )
-                              ],
+                                  )
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                    )
-                  ]
-                ),
-              ),
+                        )
+                      ]
+                    ),
+                  ),
+                );
+              }
             );
           }
         ),
