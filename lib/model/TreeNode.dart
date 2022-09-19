68d12341a0583904c72cdf38b46343b5d6c3a37a
CaptainDario <51273483+CaptainDario@users.noreply.github.com>
2022-09-19 19:11:49 +0200

    added new functionality

--- lib/model/TreeNode.dart
+++ lib/model/TreeNode.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 
 
 
@@ -10,6 +11,8 @@ class TreeNode<T> {
   List<TreeNode<T>> children = [];
   /// the parent TreeNode of this TreeNode, if null this is the root
   TreeNode<T>? parent;
+  /// the level of this node in the tree, 0 means it is the root
+  int level = 0;
 
 
   TreeNode (T value){
@@ -20,14 +23,139 @@ class TreeNode<T> {
   void add (TreeNode<T> newNode) {
     children.add(newNode);
     newNode.parent = this;
+    
+    // update the level information for the whole subtree
+    forEachBreadthFirst((node) {
+      if(node.parent != null)
+        node.level = node.parent!.level + 1;
+      else
+        node.level = 1;
+    });
   }
 
   /// Traverse the tree in depth first order
-  void forEachDepthFirst(void Function(TreeNode<T> node) performAction) {
+  void forEachDepthFirst(void Function(TreeNode node) performAction) {
     performAction(this);
     for (final child in children) {
       child.forEachDepthFirst(performAction);
     }
+  }
+
+  /// Traverse the tree in breadth first order
+  void forEachBreadthFirst(void Function(TreeNode node) performAction){
+
+    Queue queue = Queue();
+    queue.add(this);
+
+    while (queue.isNotEmpty){
+
+      performAction(queue.first);
+
+      for (final child in queue.first.children) {
+        queue.add(child);
+      }
+      queue.removeFirst();
+    }
+  }
+
+  /// Traverse the tree in pre order
+  void forEachPreOrder(void Function(TreeNode node) performAction){
+
+    for (var child in children) {
+      child.forEachPreOrder(performAction);
+    }
+    
+    performAction(this);
+  }
+
+  /// Searches the `query` in the tree. If the tree contains multiple identical
+  /// nodes, returns the first one found. If the tree does not contain the 
+  /// `query` returns null
+  TreeNode<T>? find(TreeNode<T> query){
+    TreeNode<T>? result = null;
+    
+    Queue queue = Queue();
+    queue.add(this);
+
+    while (queue.isNotEmpty){
+
+      if(queue.first == query){
+        result = query;
+        break;
+      }
+
+      for (final child in queue.first.children) {
+        queue.add(child);
+      }
+      queue.removeFirst();
+    }
+
+    return result;
+  }
+
+  /// Copies the structure of this tree to a new tree
+  /// All elements of the new tree will be initialized with `initValue`
+  /// The copy will be returned;
+  /// 
+  /// A function can be used to dynamically generate the new values of the tree
+  /// the current child is passed to the function
+  TreeNode copyStructure(dynamic Function(TreeNode oldChild) initValue){
+
+    TreeNode copiedTree = _copyChildStructure(initValue);
+    copiedTree.value = copiedTree.value(this);
+    return copiedTree;
+  }
+
+  /// Function to recursively copy the child tree
+  /// Should only be called from `copyStructure`
+  TreeNode _copyChildStructure(dynamic Function(TreeNode oldChild) initValue){
+
+    TreeNode newTree = TreeNode(initValue);
+
+    for (var child in this.children) {
+      TreeNode newChild = child._copyChildStructure(initValue);
+      newChild.value = newChild.value(child);
+      newTree.add(newChild);
+    }
+    
+    return newTree;
+  }
+
+  /// Copies the tree to a new tree and returns it
+  TreeNode copy(){
+    TreeNode newTree = TreeNode(this.value);
+
+    for (var child in this.children) {
+      print(child.value);
+      newTree.add(child.copy());
+    }
+
+    return newTree;
+  }
+
+  @override
+  String toString() {
+    return this.value.toString();
+  }
+
+  List<T> toList() {
+    List<T> graphList = [];
+
+    forEachBreadthFirst((node) {
+      graphList.add(node.value);
+    });
+
+    return graphList;
+  }
+
+  List<TreeNode> toTreeNodeList() {
+    List<TreeNode> graphList = [];
+
+    forEachBreadthFirst((node) {
+      graphList.add(node);
+    });
+
+    return graphList;
   }
 
 }
