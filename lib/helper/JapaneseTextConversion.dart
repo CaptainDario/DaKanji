89fcc9f97b10a4ea44f60c1ea7e84c9dbe7ffe63
CaptainDario <51273483+CaptainDario@users.noreply.github.com>
2022-10-06 11:55:06 +0200

    fixed: use parameter not fixed value

--- lib/helper/JapaneseTextConversion.dart
+++ lib/helper/JapaneseTextConversion.dart
@@ -4,11 +4,11 @@
 /// Given the list of string `words` removes all kana from it and returns a list
 /// containing all kanji
 List<String> removeKana(List<String> words) {
-  List<String> k = ["羨ましい", "鬱", "美しい自然", "羨ましい"];
   
   RegExp regExp = RegExp("([\u3040-\u30ff]*)");
   
-  var allKanji = k.map((String s) => s.replaceAll(regExp, "")).toList().join("").split("");   
+  var allKanji = words.map((String s) => 
+    s.replaceAll(regExp, "")).toList().join("").split("");   
   var uniqueKanji = allKanji.toSet().toList();
   
   return uniqueKanji;
