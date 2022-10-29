792695f5a210b1c7bcad08626a7197de92be640f
dario windows desktop <daapplab@gmail.com>
2022-10-29 12:46:29 +0200

    added: function to for extracting only kanji

--- lib/helper/japanese_text_conversion.dart
+++ lib/helper/japanese_text_conversion.dart
@@ -13,3 +13,22 @@ List<String> removeKana(List<String> words) {
   
   return uniqueKanji;
 }
+
+/// Given the list of string `words` removes all characters that are not kanji
+///  from it and returns a list containing all kanji
+List<String> removeAllButKanji(List<String> words) {
+  
+  List<String> uniqueKanji = [];
+
+  RegExp regExp = RegExp("([\u4e00-\u9faf]|[\u3400-\u4dbf])");
+  
+  for (String word in words) {
+    for (int i = 0; i < word.length; i++) {
+      if(!uniqueKanji.contains(word[i]) && regExp.hasMatch(word[i])){
+        uniqueKanji.add(word[i]);
+      }
+    }
+  }
+  
+  return uniqueKanji;
+}
