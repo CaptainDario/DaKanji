f76ac23bff8879640fbb557a700b8027ada3c372
dario windows desktop <daapplab@gmail.com>
2022-10-29 12:46:06 +0200

    fixed: recognize hiragana and katakana

--- lib/helper/japanese_text_conversion.dart
+++ lib/helper/japanese_text_conversion.dart
@@ -5,7 +5,7 @@
 /// containing all kanji
 List<String> removeKana(List<String> words) {
   
-  RegExp regExp = RegExp("([\u3040-\u30ff]*)");
+  RegExp regExp = RegExp("([\u3040-\u309f]|[\u3040-\u30ff])");
   
   var allKanji = words.map((String s) => 
     s.replaceAll(regExp, "")).toList().join("").split("");   
