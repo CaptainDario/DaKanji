9bb06bddba8d3726731ed2d4ef6923a0cb7bce0d
dario windows desktop <daapplab@gmail.com>
2022-11-10 21:09:26 +0100

    separated regex and string

--- lib/helper/japanese_text_processing.dart
+++ lib/helper/japanese_text_processing.dart
@@ -1,16 +1,18 @@
-// Regex that matches any Kana character
-String kanaRegex = "([\u3040-\u309f]|[\u3040-\u30ff])";
-// Regex that matches any Kanji character
-String kanjiRegex = "([\u4e00-\u9faf]|[\u3400-\u4dbf])";
+/// String that can be used for Regex that matches any Kana character
+String kanaRegexString = "([\u3040-\u309f]|[\u3040-\u30ff])";
+/// Regex that matches any Kana character
+RegExp kanaRegex = RegExp(kanaRegexString);
+/// String that can be used for Regex that matches any Kanji character
+String kanjiRegexString = "([\u4e00-\u9faf]|[\u3400-\u4dbf])";
+/// Regex that matches any Kanji character
+RegExp kanjiRegex = RegExp(kanjiRegexString);
 
 /// Given the list of string `words` removes all kana from it and returns a list
 /// containing all kanji
 List<String> removeKana(List<String> words) {
   
-  RegExp regExp = RegExp(kanaRegex);
-  
   var allKanji = words.map((String s) => 
-    s.replaceAll(regExp, "")).toList().join("").split("");   
+    s.replaceAll(kanaRegex, "")).toList().join("").split("");   
   var uniqueKanji = allKanji.toSet().toList();
   
   return uniqueKanji;
@@ -21,12 +23,10 @@ List<String> removeKana(List<String> words) {
 List<String> removeAllButKanji(List<String> words) {
   
   List<String> uniqueKanji = [];
-
-  RegExp regExp = RegExp(kanjiRegex);
   
   for (String word in words) {
     for (int i = 0; i < word.length; i++) {
-      if(!uniqueKanji.contains(word[i]) && regExp.hasMatch(word[i])){
+      if(!uniqueKanji.contains(word[i]) && kanjiRegex.hasMatch(word[i])){
         uniqueKanji.add(word[i]);
       }
     }
