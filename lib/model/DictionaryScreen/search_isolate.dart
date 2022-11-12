33feff218fad73b5ab00079406282ba47ed317ac
dario windows desktop <daapplab@gmail.com>
2022-11-12 10:44:12 +0100

    moved to search_isolate

--- lib/model/DictionaryScreen/search_isolate.dart
+++ lib/model/DictionaryScreen/search_isolate.dart
@@ -158,3 +158,106 @@ Future<void> _readAndParseJsonService(SendPort p) async {
   print('Spawned isolate finished.');
   Isolate.exit();
 }
+
+/// Sorts a list of Jmdict entries given a query text. The order is determined
+/// by those sorting criteria:
+/// 
+/// 1. Full Match > Match at the beginning > Match somwhere in the word
+///    Those three categories are sorted individually and merged in the end
+///   2.  sort inside each category based on <br/>
+List<isar_jm.Entry> sortJmdictList(
+  List<isar_jm.Entry> entries, 
+  String queryText,
+  List<String> languages)
+  {
+
+  /// lists with three sub lists
+  /// 0 - full matchs 
+  /// 1 - matchs starting at the word beginning 
+  /// 2 - other matches
+  List<List<isar_jm.Entry>> matches = [[], [], []];
+  List<List<int>> matchIndices = [[], [], []];
+  String queryTextHira = KanaKit().toHiragana(queryText);
+
+  // iterate over the entries and create a ranking for each 
+  for (isar_jm.Entry entry in entries) {
+    // KANJI
+    Tuple3 result = rankMatches(entry.kanjis, queryText);
+    
+    // KANA
+    if(result.item1 == -1)
+      result = rankMatches(entry.readings, queryTextHira);
+    
+    // MEANING
+    // filter all langauges that are selected in the settings and join them to 
+    // a list
+    if(result.item1 == -1){
+      List<String> k = entry.meanings.where((isar_jm.LanguageMeanings e) =>
+          languages.contains(e.language)
+        ).map((isar_jm.LanguageMeanings e) => 
+          e.meanings!
+        ).expand((e) => e).toList();
+      result = rankMatches(k, queryText);
+    }
+
+    if(result.item1 != -1){
+      matches[result.item1].add(entry);
+      matchIndices[result.item1].add(result.item3);
+    }
+  }
+
+  matches[0] = sortEntriesByInts(matches[0], matchIndices[0]);
+  matches[1] = sortEntriesByInts(matches[1], matchIndices[1]);
+  matches[2] = sortEntriesByInts(matches[2], matchIndices[2]);
+  return matches.expand((element) => element).toList();
+}
+
+/// Sorts a string list based on `queryText`. The sorting criteria are
+/// explained by `sortJmdictList`.
+///
+/// Returns a Tuple with the structure: <br/>
+///   1 - if it was a full (0), start(1) or other(2) match <br/>
+///   2 - how many characters are in the match but not in `queryText` <br/>
+///   3 - the index where the search matched <br/>
+Tuple3<int, int, int> rankMatches(List<String> matches, String queryText) {   
+
+  int result = -1, lenDiff = -1;
+
+  // check if the word contains the query
+  int matchIndex = matches.indexWhere((element) => element.contains(queryText));
+  if(matchIndex != -1){
+    // check kanji for full match
+    if(matches[matchIndex] == queryText){
+      result = 0;
+    }
+    // does the found dict entry start with the search term
+    else if(matches[matchIndex].startsWith(queryText)){
+      result = 1;
+    }
+    // how many additional characters does this entry include
+    else {
+      result = 2;
+    }
+    /// calculatt the difference in length between the query and the result
+    lenDiff = matches[matchIndex].length - queryText.length;
+  }
+
+  return Tuple3(result, lenDiff, matchIndex);
+}
+
+/// Sorts list `a` based on the values in `b` and returns it.
+/// 
+/// Throws an exception if the lists do not have the same length.
+List<isar_jm.Entry> sortEntriesByInts(List<isar_jm.Entry> a, List<int> b){
+
+  assert (a.length == b.length);
+
+  List<Tuple2<isar_jm.Entry, int>> combined = List.generate(b.length,
+    (i) => Tuple2(a[i], b[i])
+  );
+  combined.sort(
+    (_a, _b) => _a.item2 - _b.item2
+  );
+
+  return  combined.map((e) => e.item1).toList();
+}
