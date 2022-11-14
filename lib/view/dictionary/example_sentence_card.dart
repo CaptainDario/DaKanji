c734f423fc3e4b41a24d6af1077480401d83a32f
dario windows desktop <daapplab@gmail.com>
2022-11-14 14:25:56 +0100

    do not show example if == null

--- lib/view/dictionary/example_sentence_card.dart
+++ lib/view/dictionary/example_sentence_card.dart
@@ -3,15 +3,12 @@ import 'package:flutter/material.dart';
 
 class ExampleSentenceCard extends StatefulWidget {
   const ExampleSentenceCard(
-    this.sentence,
-    this.translations,
+    this.sentences,
     {Key? key}
   ) : super(key: key);
 
-  /// the japanese example sentence
-  final String sentence;
-  /// a list containing all translations that should be shown
-  final List<String> translations;
+  /// the example sentence
+  final List<String>? sentences;
 
   @override
   State<ExampleSentenceCard> createState() => _ExampleSentenceCardState();
@@ -20,25 +17,26 @@ class ExampleSentenceCard extends StatefulWidget {
 class _ExampleSentenceCardState extends State<ExampleSentenceCard> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 150,
-      child: Card(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                widget.sentence
-              ),
-              Text(
-                widget.translations.join("\n")
-              ),
-            ],
-          ),
-        )
-      ),
-    );
+
+    if(widget.sentences == null)
+      return Container();
+    else
+      return SizedBox(
+        height: 150,
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.sentences!.join("\n")
+                ),
+              ],
+            ),
+          )
+        ),
+      );
   }
 }
