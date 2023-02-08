4ee54531af273e00da97f66e71f259a40ec63399
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-02-08 19:06:39 +0100

    moved plugins to DaKanji-Dependencies

--- plugins/flutter_appavailability/ios/Classes/AppAvailability.m
+++
@@ -1,8 +0,0 @@
-#import "AppAvailability.h"
-#import <flutter_appavailability/flutter_appavailability-Swift.h>
-
-@implementation AppAvailability : NSObject 
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterPlugin registerWithRegistrar:registrar];
-}
-@end
