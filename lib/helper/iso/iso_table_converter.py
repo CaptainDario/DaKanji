36f0aad43cff7236d7edd34c1b9e4dd6abe526f0
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-02-15 22:05:22 +0100

    updated iso codes

--- lib/helper/iso/iso_table_converter.py
+++ lib/helper/iso/iso_table_converter.py
@@ -1,3 +1,4 @@
+### Data source: https://iso639-3.sil.org/code_tables/download_tables#639-3%20Code%20Set
 
 
 
@@ -7,27 +8,35 @@ iso639_2B = "iso639_2B"
 iso639_3  = "iso639_3"
 
 is_warning = """/// 
-/// Note: ISO code `is` has been renamed to `is_` because it conflicts with dart's 
-/// keyword `is`
+/// Note: Some ISO codes have been because of conflicts with dart's keywords.
+/// `is` -> `is_` 
+/// `new` -> `is_`
+/// `for` -> `for_`
 """
 
 def main():
 
-    # read the iso table from wikipedia
+    # read the iso table from sil.org
     content = []
-    with open("./lib/helper/iso/iso_table.txt", mode="r", encoding="utf-8") as f:
+    with open("./lib/helper/iso/iso-639-3_20230123.tab", mode="r", encoding="utf-8") as f:
+        #skip header
+        f.readline()
         for line in f.readlines():
             line = line.replace("\n", "")
             line = line.split("\t")
 
-            # remove the plus annotations and rename the code `is` to `is_`
-            for c, i in enumerate(line):
-                if("+" in i):
-                    line[c] = i[:(i.index('+')) - 1]
-                if("is" == i):
-                    line[c] = "is_"
+            # remove scope, language and comment field fields
+            del line[4:6]; del line[5]
 
-            content.append(line)
+            # rename codes that conflict with dart keywords, or that are not valid in strings
+            for c, i in enumerate(line):
+                if(i in ["is", "new", "try", "for", "var"]):
+                    line[c] = i + "_"
+                if("'" in i or "’" in i):
+                    line[c] = line[c].replace("'", "").replace("’", "")
+
+            # append the data in the format [Ref_name, iso_1, iso_2T, iso_2B, iso_3]
+            content.append(line[::-1])
 
             
 
@@ -45,31 +54,53 @@ def main():
             if(cnt == 0):
                 f.write(is_warning)
             f.write(f"enum {iso}" + "{\n\t")
-            for c, i in enumerate(content, start=1):
+            
+            c = 1
+            for i in content:
+                if(i[cnt+1] == ""):
+                    continue
 
                 f.write(str(i[cnt+1]) + ", ")
                 
                 if(c % 15 == 0 and c > 0):
                     f.write("\n\t")
+                c += 1
             f.write("\n}\n\n")
 
             f.write(f"/// Map to convert ISO 639-1, 639-1, 639-1, 639-1, to an enum entry of `{iso}`\n{is_warning}")
             f.write(f"const Map<String, {iso}> isoTo{iso} = const" + " {\n")
+            c = 1
             for i in content:
                 i_new = list(dict.fromkeys(i[1:5]))
                 for j in range(len(i_new)):
+                    if(i[cnt+1] == "" or i_new[j] == ""):
+                        continue
                     f.write(f"\t'{i_new[j]}' : {iso}.{i[cnt+1]},")
-                f.write("\n")
+                    c += 1
+                    if(c % 4 == 0 and c > 0):
+                        f.write("\n\t")
             f.write("\n};\n\n\n")
 
         # create a Map from ISO code to full language name
         f.write(f"/// Map to convert ISO 639-1, 639-1, 639-1, 639-1, to an language string\n")
         f.write(f"const Map<Enum, String> isoToLanguage = const" + " {\n")
+        cnt = 0
         for c in content:
-            f.write(f"\t{iso639_1}.{c[1]} : '{c[0]}', ")
-            f.write(f"{iso639_2T}.{c[2]} : '{c[0]}', \n")
-            f.write(f"\t{iso639_2B}.{c[3]} : '{c[0]}', ")
-            f.write(f"{iso639_3}.{c[4]} : '{c[0]}',\n\n")
+            f.write("\t")
+            if(c[1] != ""):
+                f.write(f"{iso639_1}.{c[1]} : '{c[0]}', ")
+                cnt += 1
+            if(c[2] != ""):
+                f.write(f"{iso639_2T}.{c[2]} : '{c[0]}', ")
+                cnt += 1
+            if(c[3] != ""):
+                f.write(f"\t{iso639_2B}.{c[3]} : '{c[0]}', ")
+                cnt += 1
+            if(c[4] != ""):
+                f.write(f"{iso639_3}.{c[4]} : '{c[0]}', ")
+                cnt += 1
+            if(cnt % 4 == 0 and cnt > 0):
+                f.write("\n")
         f.write("};")
 
 if __name__ == "__main__":
