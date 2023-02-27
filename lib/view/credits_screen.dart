2b774d44af584969e3705319f6a23b61de027a65
dario windows desktop <daapplab@gmail.com>
2023-02-27 23:30:57 +0100

    updated credits

--- lib/view/credits_screen.dart
+++ lib/view/credits_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 import 'package:da_kanji_mobile/globals.dart';
 import 'package:da_kanji_mobile/locales_keys.dart';
