cba3272e4243799dab0e06dd48dee4ae15816f89
dario windows desktop <daapplab@gmail.com>
2023-02-27 23:29:43 +0100

    fixed: credits links not working

--- lib/view/credits_screen.dart
+++ lib/view/credits_screen.dart
@@ -3,9 +3,11 @@ import 'package:flutter/services.dart';
 
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'package:da_kanji_mobile/globals.dart';
 import 'package:da_kanji_mobile/locales_keys.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 
 
@@ -45,6 +47,9 @@ class _CreditsScreenState extends State<CreditsScreen> {
                       return Markdown(
                         data: snapshot.data!,
                         selectable: false,
+                        onTapLink: (text, href, title) {
+                          launchUrlString(href!);
+                        },
                       );
                     } else {
                       return Container();
