8762dad6afdae8777daf820040a40c7a97661fc0
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-03-12 12:03:42 +0100

    reimplementation of basic tree structure

--- lib/model/tree_node.dart
+++ lib/model/tree_node.dart
@@ -1,161 +1,234 @@
 import 'dart:collection';
+import 'dart:convert';
+
+import 'package:json_annotation/json_annotation.dart';
 
 
 
-/// Class representing one TreeNode of a Tree datastructure
+enum TreeTraversalMode{
+  /// Traverse the tree in breadth first order
+  BFS,
+  /// Traverse the tree in depth first order
+  DFS,
+}
+
 class TreeNode<T> {
 
   /// the value of this node
-  late T value;
+  T value;
   /// a list containing all children of this TreeNode
-  List<TreeNode<T>> children = [];
+  List<TreeNode<T>> _children = [];
+  /// a list containing all children of this TreeNode
+  get children => _children;
   /// the parent TreeNode of this TreeNode, if null this is the root
   TreeNode<T>? parent;
   /// the level of this node in the tree, 0 means it is the root
-  int level = 0;
+  int _level = 0;
+  /// the level of this node in the tree, 0 means it is the root
+  int get level => _level;
+
+  @JsonKey(ignore: true)
+  late Map<TreeTraversalMode, Iterable<TreeNode<T>>> _iterDict = {
+    TreeTraversalMode.BFS: BFS(),
+    TreeTraversalMode.DFS: DFS(),
+  };
 
 
-  TreeNode (T value){
-    value = value;
-  }
+  TreeNode (this.value, {int level = 0});
 
   /// adds newNode as a child to this node
-  void add (TreeNode<T> newNode) {
+  void addChild (TreeNode<T> newNode) {
     children.add(newNode);
     newNode.parent = this;
     
     // update the level information for the whole subtree
-    forEachBreadthFirst((node) {
-      if(node.parent != null) {
-        node.level = node.parent!.level + 1;
-      } else {
-        node.level = 1;
-      }
-    });
+    for (final node in DFS()) {
+      node._level = node.parent!.level + 1;
+    }
   }
 
-  /// Traverse the tree in depth first order
-  void forEachDepthFirst(void Function(TreeNode node) performAction) {
-    performAction(this);
-    for (final child in children) {
-      child.forEachDepthFirst(performAction);
+  /// Adds all nodes in `newNodes` as children to this node
+  void addChildren (List<TreeNode<T>> newNodes) {
+    children.addAll(newNodes);
+    for (final node in newNodes) {
+      node.parent = this;
     }
+
+    // update the level information for the whole subtree
+    for (final node in DFS()) {
+      node._level = node.parent!.level + 1;
+    }
+  }
+  
+  /// removes `node` from the children of this node and returns it
+  TreeNode<T> removeChild (TreeNode<T> node) {
+    children.remove(node);
+    node.parent = null;
+
+    return node;
+  }
+
+  /// remove all nodes given in `nodes` from the children of this node
+  /// If there are multiple nodes with the same value, only the first one is
+  /// removed
+  /// Returns a list of all removed nodes
+  List<TreeNode<T>> removeChildren (List<TreeNode<T>> nodes) {
+
+    List<TreeNode<T>> removed = [];
+
+    for (final node in nodes) {
+      removed.add(children.remove(node));
+      node.parent = null;
+    }
+
+    return removed;
+  }
+
+  /// removes all nodes from the children of this node and returns them
+  List<TreeNode<T>> clearChildren () {
+
+    List<TreeNode<T>> tmp = children;
+
+    for (final node in children) {
+      node.parent = null;
+    }
+    children.clear();
+
+    return tmp;
+  }
+
+  /// Traverses the tree and returns the top level node
+  TreeNode<T> getRoot() {
+    TreeNode<T> node = this;
+
+    while (node.parent != null) {
+      node = node.parent!;
+    }
+
+    return node;
+  }
+
+  /// returns a list with all nodes to this node
+  /// has the structure: [root, n1, n2, ..., this]
+  List<TreeNode> getPath() {
+    
+    List<TreeNode<T>> path = [this];
+
+    while (path.first.parent != null) {
+      if(path.first.parent != null)
+        path.insert(0, path.last.parent!);
+    }
+
+    return path;
   }
 
   /// Traverse the tree in breadth first order
-  void forEachBreadthFirst(void Function(TreeNode node) performAction){
-
-    Queue queue = Queue();
-    queue.add(this);
-
-    while (queue.isNotEmpty){
-
-      performAction(queue.first);
-
-      for (final child in queue.first.children) {
-        queue.add(child);
-      }
-      queue.removeFirst();
+  Iterable<TreeNode<T>> BFS() sync* {
+    Queue<TreeNode<T>> queue = Queue()..add(this);
+    
+    while (queue.isNotEmpty) {
+      TreeNode<T> node = queue.removeFirst();
+      yield node;
+      queue.addAll(node.children);
     }
   }
 
-  /// Traverse the tree in pre order
-  void forEachPreOrder(void Function(TreeNode node) performAction){
-
-    for (var child in children) {
-      child.forEachPreOrder(performAction);
+  /// Traverse the tree in depth first order
+  Iterable<TreeNode<T>> DFS() sync* {
+    for (TreeNode<T> node in _children) {
+      yield node;
+      yield* node.DFS();
     }
-    
-    performAction(this);
   }
 
   /// Searches the `query` in the tree. If the tree contains multiple identical
   /// nodes, returns the first one found. If the tree does not contain the 
-  /// `query` returns null
-  TreeNode<T>? find(TreeNode<T> query){
-    TreeNode<T>? result;
-    
-    Queue queue = Queue();
-    queue.add(this);
+  /// `query` returns null.
+  /// `TreeNode`s are compared using their value only.
+  /// `mode` determines the algorithm used to traverse the tree.
+  TreeNode<T>? find(TreeNode<T> query, {TreeTraversalMode mode = TreeTraversalMode.BFS}){
 
-    while (queue.isNotEmpty){
+    var iter = _iterDict[mode]!;
 
-      if(queue.first == query){
-        result = query;
-        break;
+    for (var node in iter) {
+      if(node.value == query.value){
+        return node;
       }
-
-      for (final child in queue.first.children) {
-        queue.add(child);
-      }
-      queue.removeFirst();
     }
 
-    return result;
+    return null;
   }
 
-  /// Copies the structure of this tree to a new tree
-  /// All elements of the new tree will be initialized with `initValue`
-  /// The copy will be returned;
-  /// 
-  /// A function can be used to dynamically generate the new values of the tree
-  /// the current child is passed to the function
-  TreeNode copyStructure(dynamic Function(TreeNode oldChild) initValue){
-
-    TreeNode copiedTree = _copyChildStructure(initValue);
-    copiedTree.value = copiedTree.value(this);
-    return copiedTree;
+  /// Copies the tree to a new tree and returns it.
+  TreeNode<T> copy(){
+    return _copy(this);
   }
 
-  /// Function to recursively copy the child tree
-  /// Should only be called from `copyStructure`
-  TreeNode _copyChildStructure(dynamic Function(TreeNode oldChild) initValue){
-
-    TreeNode newTree = TreeNode(initValue);
-
-    for (var child in children) {
-      TreeNode newChild = child._copyChildStructure(initValue);
-      newChild.value = newChild.value(child);
-      newTree.add(newChild);
-    }
+  /// Copies the tree to a new tree and returns it, `copy` is just the public
+  /// interface to this function, so that it is not neceessary to call it with
+  /// the root node as argument.
+  TreeNode<T> _copy(TreeNode<T> root){
     
-    return newTree;
-  }
+    if (root.children.isEmpty) return TreeNode(root.value);
 
-  /// Copies the tree to a new tree and returns it
-  TreeNode copy(){
-    TreeNode newTree = TreeNode(value);
-
-    for (var child in children) {
-      newTree.add(child.copy());
-    }
-
-    return newTree;
+    return TreeNode(root.value)..children.addAll(
+      root.children.map((e) => e._copy(e))
+    );
   }
 
   @override
   String toString() {
-    return value.toString();
+    return "$value, level: $level, children: ${children.map((e) => e.value)}";
   }
 
-  List<T> toList() {
-    List<T> graphList = [];
+  /// Converts this tree to a string by traversing it and printing the values
+  /// of each layer
+  String toTreeString() {
+    String treeString = '';
 
-    forEachBreadthFirst((node) {
-      graphList.add(node.value);
-    });
+    int previousLevel = 0;
+    for (var node in BFS()) {
+      if (node.level > previousLevel) {
+        treeString += "\n${node.level}: ";
+        previousLevel = node.level;
+      }
+      treeString += "${node.value} c: ${node.children.map((e) => e.value)}\t";
+    }
 
-    return graphList;
+    return treeString;
   }
 
-  List<TreeNode> toTreeNodeList() {
-    List<TreeNode> graphList = [];
+  /// Converts this tree to a list by traversing it in the order specified by
+  /// `mode`, finally returns the list.
+  List<T> toList({TreeTraversalMode mode = TreeTraversalMode.BFS}) {
+    
+    List<T> treeList = [];
+    
+    for (var node in _iterDict[mode]!) {
+      treeList.add(node.value);
+    }
 
-    forEachBreadthFirst((node) {
-      graphList.add(node);
-    });
-
-    return graphList;
+    return treeList;
   }
 
+  factory TreeNode.fromJson(Map<String, dynamic> json) {
+
+    T value = json['value'] as T;
+    int level = json['level'] as int;
+    List<TreeNode<T>> children = (json['children'] as List<dynamic>)
+      .map((e) => TreeNode<T>.fromJson(e as Map<String, dynamic>))
+      .toList();
+
+    return TreeNode<T>(value)
+      .._level = level
+      .._children = children;
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      'value': value,
+      'level': level,
+      'children': children.map((e) => e.toJson()).toList(),
+    };
+  }
 }
