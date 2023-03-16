91077d47d0b9ecbce8b168868bc6672390c94650
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-03-16 21:12:01 +0100

    added arugments to show an initial text in text screen

--- lib/model/navigation_arguments.dart
+++ lib/model/navigation_arguments.dart
@@ -7,18 +7,22 @@ class NavigationArguments{
   /// was the route opened by the navigation drawer
   final bool navigatedByDrawer;
 
-  // DRAWING
+  // DRAWING SCREEN
   /// prefix that should be prepended to every search query
   final String drawSearchPrefix;
   /// postfix that should be appended to every search query
   final String drawSearchPostfix;
 
-  //DICT
+  // DICT SCREEN
   /// query that should be searched when navigating the dictionary screen
   final String initialDictSearch;
   /// id of the entry that should be shown when navigating to the dictionary
   /// screen
   final int? initialEntryId;
+
+  // TEXT SCREEN
+  final String? initialText;
+
 
   NavigationArguments(
     this.navigatedByDrawer,
@@ -28,6 +32,8 @@ class NavigationArguments{
 
       this.initialDictSearch = "",
       this.initialEntryId,
+
+      this.initialText
     }
   );
 
