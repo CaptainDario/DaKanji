2d6f2778765403d93a16bbcd44bfb5a592deb05c
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-04-18 21:21:11 +0200

    updated to markdown

--- lib/view/manual/manual_text.dart
+++ lib/view/manual/manual_text.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:easy_localization/easy_localization.dart';
-import 'package:simple_html_css/simple_html_css.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 import 'package:da_kanji_mobile/locales_keys.dart';
-import 'package:da_kanji_mobile/helper/color_conversion.dart';
-import 'package:da_kanji_mobile/helper/part_of_speech.dart';
 
 
 
@@ -17,12 +15,10 @@ class ManualTextScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return HTML.toRichText(
-      context, 
-      manualTextScreenText,
-      defaultTextStyle: TextStyle(
-        color: Theme.of(context).textTheme.bodyLarge!.color
-      )
+    return SingleChildScrollView(
+      child: MarkdownBody(
+        data: LocaleKeys.ManualScreen_title.tr(),
+      ),
     );
   }
 }
