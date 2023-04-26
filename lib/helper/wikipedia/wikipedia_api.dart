d92794d839f2fcafb17836ecad380a26140bfb53
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-04-26 18:23:38 +0200

    function to query wikipedia

---
+++ lib/helper/wikipedia/wikipedia_api.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+
+
+/// Queries the wikipedia API and returns the definition of the `topic`.
+/// Returns the summary of the topic if it exists, otherwise an empty string.
+/// Uses the given locale to query the wikipedia API.
+Future<String> getWikipediaDefinition(String topic, String locale) async {
+
+  String url = "https://$locale.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=$topic";
+  Response response;
+
+  try{
+    response = await get(Uri.parse(url));
+  }
+  catch (e) {
+    print(e);
+    return "";
+  }
+
+  if(response.statusCode == 200){
+    Map<String, dynamic> json = jsonDecode(response.body);
+    String? extract = json["query"]["pages"].values.first["extract"];
+    if(extract != null){
+      return extract;
+    }
+  }
+
+  return "";
+
+}
