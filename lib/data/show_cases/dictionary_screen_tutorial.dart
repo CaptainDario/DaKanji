ddb13993fb43a7d98972c8e9347b45b81ecbf3d2
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-04-29 16:47:22 +0200

    added new buttons to tutorial

--- lib/data/show_cases/dictionary_screen_tutorial.dart
+++ lib/data/show_cases/dictionary_screen_tutorial.dart
@@ -10,8 +10,11 @@ class DictionaryScreenTutorial extends Tutorial {
 
   // all tutorial steps that belong to the DictionaryScreen
   late FocusNode searchInputStep;
+  late FocusNode searchInputWildcardsStep;
   late FocusNode searchInputClearStep;
   late FocusNode searchInputDrawStep;
+  late FocusNode searchFilterStep;
+  late FocusNode searchRadicalStep;
   late FocusNode wordTabStep;
   late FocusNode kanjiTabStep;
   late FocusNode examplesTabStep;
@@ -25,12 +28,18 @@ class DictionaryScreenTutorial extends Tutorial {
       "",
       "",
       "",
+      "",
+      "",
+      ""
     ];
     bodies = [
       LocaleKeys.DictionaryScreen_tutorial_begin_text.tr(),
       LocaleKeys.DictionaryScreen_tutorial_search_input_text.tr(),
+      LocaleKeys.DictionaryScreen_tutorial_search_input_wildcards_text.tr(),
       LocaleKeys.DictionaryScreen_tutorial_search_input_clear_text.tr(),
       LocaleKeys.DictionaryScreen_tutorial_search_input_draw_text.tr(),
+      LocaleKeys.DictionaryScreen_tutorial_search_filter_text.tr(),
+      LocaleKeys.DictionaryScreen_tutorial_search_radical_text.tr(),
       LocaleKeys.DictionaryScreen_tutorial_word_tab.tr(),
       LocaleKeys.DictionaryScreen_tutorial_kanji_tab.tr(),
       LocaleKeys.DictionaryScreen_tutorial_examples_tab.tr(),
@@ -38,12 +47,15 @@ class DictionaryScreenTutorial extends Tutorial {
 
     initTutorial();
 
-    searchInputStep      = focusNodes![1];
-    searchInputClearStep = focusNodes![2];
-    searchInputDrawStep  = focusNodes![3];
-    wordTabStep          = focusNodes![4];
-    kanjiTabStep         = focusNodes![5];
-    examplesTabStep      = focusNodes![6];
+    searchInputStep          = focusNodes![1];
+    searchInputWildcardsStep = focusNodes![2];
+    searchInputClearStep     = focusNodes![3];
+    searchInputDrawStep      = focusNodes![4];
+    searchFilterStep         = focusNodes![5];
+    searchRadicalStep        = focusNodes![6];
+    wordTabStep              = focusNodes![7];
+    kanjiTabStep             = focusNodes![8];
+    examplesTabStep          = focusNodes![9];
   } 
 
 }
