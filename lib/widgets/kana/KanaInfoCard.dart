03b56254fdff63a6b5a7bc777bb3c311fd93ebf4
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-05-13 20:22:15 +0200

    removed: unused function

--- lib/widgets/kana/KanaInfoCard.dart
+++ lib/widgets/kana/KanaInfoCard.dart
@@ -176,11 +176,4 @@ class _KanaInfoCardState extends State<KanaInfoCard> {
     );
   }
 
-  /// Calculate text size
-  Size _calculateTextSize(String text, TextStyle style) {
-    final TextPainter textPainter = TextPainter(
-        text: TextSpan(text: text, style: style), maxLines: 1, textDirection: TextDirection.ltr)
-      ..layout(minWidth: 0, maxWidth: double.infinity);
-    return textPainter.size;
-  }
 }
