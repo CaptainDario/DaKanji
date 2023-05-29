44ac60799da93ef4deb55ab0fde4f09ce03d0c87
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-05-29 17:17:46 +0200

    added: responsiveness

--- lib/widgets/dictionary/filter_popup_body.dart
+++ lib/widgets/dictionary/filter_popup_body.dart
@@ -45,70 +45,83 @@ class _FilterPopupBodyState extends State<FilterPopupBody> {
 
   @override
   Widget build(BuildContext context) {
+
+    int crossAxisCount = max([MediaQuery.of(context).size.width ~/ 200, 1])!;
+
     return Container(
       height: widget.height*3/4,
-      child: GridView(
+      child: GridView.builder(
         clipBehavior: Clip.hardEdge,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: (MediaQuery.of(context).size.width+600) ~/ 600 * 2,
-          childAspectRatio: MediaQuery.of(context).size.width /
-            (MediaQuery.of(context).size.height / 6),
+          crossAxisCount: crossAxisCount,
+          childAspectRatio: (MediaQuery.of(context).size.width / crossAxisCount) /
+            (MediaQuery.of(context).size.height / 10),
           crossAxisSpacing: 4,
           mainAxisSpacing: 4
         ),
-        children: [
-          for(var pair in zip([jmDictFieldsSorted.entries, jmDictPosSorted.entries]))
-            for (var item in pair)
-              item.value != ""
-                ? Container(
-                  decoration: BoxDecoration(
-                    color: selectedFilters.contains(item.key)
-                      ? g_Dakanji_green.withOpacity(0.5)
-                      : null,
-                    border: Border.all(
-                      color: Colors.grey.withOpacity(0.5),
-                    ),
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: InkWell(
-                    onTap: selectedFilters.contains(item.key)
-                      ? () {
-                        widget.searchController.text = widget.searchController.text
-                          .replaceAll("#${item.key} ", "");
-                        setState(() {
-                          selectedFilters.remove(item.key);
-                        });
-                      }
-                      : () {
-                        String newText = "#${item.key} ${widget.searchController.text}";
-                        selectedFilters.add(item.key);
-                        setState(() {
-                          widget.searchController.text = newText;
-                        });
-                      },
-                    child: Padding(
-                      padding: const EdgeInsets.all(2.0),
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          item.value,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 13,
-                            color: selectedFilters.contains(item.key)
-                              ? Colors.grey
-                              : Theme.of(context).brightness == Brightness.dark
-                                ? Colors.white
-                                : Colors.black
-                          ),
-                        ),
+        itemCount: jmDictAllFiltersSorted.length+1,
+        itemBuilder: (context, index) {
+
+          MapEntry<String, String> item = MapEntry("", "");
+
+          int cnt = 0;
+          for (MapEntry<String, String> i in jmDictAllFiltersSorted.entries) {
+            if (cnt == index) {
+              item = i;
+              break;
+            }
+            cnt++;
+          }
+
+          if(item.value != "")
+            return Container(
+              decoration: BoxDecoration(
+                color: selectedFilters.contains(item.key)
+                  ? g_Dakanji_green.withOpacity(0.5)
+                  : null,
+                border: Border.all(
+                  color: Colors.grey.withOpacity(0.5),
+                ),
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: InkWell(
+                onTap: selectedFilters.contains(item.key)
+                  ? () {
+                    widget.searchController.text = widget.searchController.text
+                      .replaceAll("#${item.key} ", "");
+                    setState(() {
+                      selectedFilters.remove(item.key);
+                    });
+                  }
+                  : () {
+                    String newText = "#${item.key} ${widget.searchController.text}";
+                    selectedFilters.add(item.key);
+                    setState(() {
+                      widget.searchController.text = newText;
+                    });
+                  },
+                child: Padding(
+                  padding: const EdgeInsets.all(2.0),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      item.value,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: selectedFilters.contains(item.key)
+                          ? Colors.grey
+                          : Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black
                       ),
                     ),
                   ),
-                )
-                : Container(),
-        ]
+                ),
+              ),
+            );
+        },
       ),
     );
   }
