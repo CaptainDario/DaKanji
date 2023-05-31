e0c0e3c4bdf7cb94e4713bf899d1e6d82f99289a
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-05-31 22:31:58 +0200

    removed unused imports

--- lib/widgets/dictionary/dictionary_search_widget.dart
+++ lib/widgets/dictionary/dictionary_search_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 import 'package:get_it/get_it.dart';
 import 'package:kana_kit/kana_kit.dart';
--- lib/widgets/kana/KanaInfoCard.dart
+++ lib/widgets/kana/KanaInfoCard.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 
 import 'package:database_builder/database_builder.dart';
