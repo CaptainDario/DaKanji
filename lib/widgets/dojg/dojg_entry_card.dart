52624e186d9bc9da4b0fc8099723959531ef020a
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-08-19 11:44:13 +0200

    widget that is shown in the list of all DoJG grammar points

---
+++ lib/widgets/dojg/dojg_entry_card.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+import 'package:da_kanji_mobile/globals.dart';
+import 'package:da_kanji_mobile/domain/dojg/dojg_entry.dart';
+
+
+
+class DojgEntryCard extends StatelessWidget {
+
+  /// The DoJG entry of this widget
+  final DojgEntry dojgEntry;
+
+
+  const DojgEntryCard(
+    this.dojgEntry,
+    {
+      super.key
+    }
+  );
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          child: Card(
+            child: InkWell(
+              onTap: () {
+                print(this.dojgEntry.grammaticalConcept);
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    Text(
+                      this.dojgEntry.grammaticalConcept,
+                      textScaleFactor: 1.5,
+                      style: TextStyle(
+                        fontFamily: g_japaneseFontFamily
+                      ),
+                    ),
+                    Text(
+                      this.dojgEntry.usage ?? "",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontFamily: g_japaneseFontFamily
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ),
+        ),
+        Positioned(
+          right: 6,
+          top: 4,
+          child: Text(
+            this.dojgEntry.volumeTag,
+            textScaleFactor: 1.25,
+          ),
+        ),
+      ],
+    );
+  }
+}
