21438cef0a21f7b44d3529aa9b3388354f8c20cc
CaptainDarioUbuntu <daapplab@gmail.com>
2023-08-29 22:35:45 +0200

    deep link

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -17,6 +17,15 @@ G_DEFINE_TYPE(MyApplication, my_application, GTK_TYPE_APPLICATION)
 // Implements GApplication::activate.
 static void my_application_activate(GApplication* application) {
   MyApplication* self = MY_APPLICATION(application);
+
+  // app links support - start
+  GList* windows = gtk_application_get_windows(GTK_APPLICATION(application));
+  if (windows) {
+    gtk_window_present(GTK_WINDOW(windows->data));
+    return;
+  }
+  //  app links support - end
+
   GtkWindow* window =
       GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(application)));
 
@@ -78,7 +87,8 @@ static gboolean my_application_local_command_line(GApplication* application, gch
   g_application_activate(application);
   *exit_status = 0;
 
-  return TRUE;
+  // App links support TRUE -> FALSE
+  return FALSE;
 }
 
 // Implements GObject::dispose.
@@ -97,8 +107,9 @@ static void my_application_class_init(MyApplicationClass* klass) {
 static void my_application_init(MyApplication* self) {}
 
 MyApplication* my_application_new() {
-  return MY_APPLICATION(g_object_new(my_application_get_type(),
-                                     "application-id", APPLICATION_ID,
-                                     "flags", G_APPLICATION_NON_UNIQUE,
-                                     nullptr));
+  // App links support
+  return MY_APPLICATION(g_object_new(
+    my_application_get_type(), "application-id", APPLICATION_ID, "flags",
+    G_APPLICATION_HANDLES_COMMAND_LINE | G_APPLICATION_HANDLES_OPEN,
+    nullptr));
 }
