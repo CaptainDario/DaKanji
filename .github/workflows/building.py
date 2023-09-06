00a788f326beb1dca0c58f74f7449307760ffc49
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-09-06 22:24:10 +0200

    build linux

--- .github/workflows/building.py
+++ .github/workflows/building.py
@@ -18,6 +18,24 @@ def set_env_flutter_version():
 
     utils.set_github_env("FLUTTER_VERSION", version)
 
+def get_latest_changes() :
+    """ Reads the Changelog from the last release and returns it.
+    """
+    with open("CHANGELOG.md", mode="r", encoding="utf8") as f:
+        changelog = f.read()
+
+    newest_changes = re.search("##(.*?)----------------", changelog, re.DOTALL).groups()[0]
+
+    return newest_changes
+
+def create_latest_changes_txt():
+    """ Creates a file called "latest_changes.txt" and 
+    writes the Changelog of the newest release to it
+    """
+    
+    with open("latest_changes.txt", mode="w+", encoding="utf8") as f:
+        f.write(get_latest_changes())   
+
 
 
 if __name__ == "__main__":
