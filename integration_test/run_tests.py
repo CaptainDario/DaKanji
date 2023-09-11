6a793cd4da7435387f3cc2951989e7647075f211
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-09-11 14:25:36 +0200

    run tests

--- integration_test/run_tests.py
+++ integration_test/run_tests.py
@@ -21,8 +21,8 @@ if __name__ == "__main__":
 
     # copy the tf lite binaries
     if platform == "linux" or platform == "linux2":
-        os.makedirs(os.path.dirname("build/linux/x64/debug/blobs/"), exist_ok=True)
-        shutil.copy("blobs/libtensorflowlite_c-linux.so", "build/linux/x64/debug/blobs/")
+        #os.makedirs(os.path.dirname("build/linux/x64/debug/blobs/"), exist_ok=True)
+        #shutil.copy("blobs/libtensorflowlite_c-linux.so", "build/linux/x64/debug/blobs/")
 
         device = "linux"
 
@@ -36,8 +36,8 @@ if __name__ == "__main__":
 
     # Windows
     elif platform == "win32":
-        os.makedirs(os.path.dirname("build/windows/runner/Debug/blobs/"), exist_ok=True)
-        shutil.copy("blobs/libtensorflowlite_c-win.dll", "build/windows/runner/Debug/blobs/")
+        #os.makedirs(os.path.dirname("build/windows/runner/Debug/blobs/"), exist_ok=True)
+        #shutil.copy("blobs/libtensorflowlite_c-win.dll", "build/windows/runner/Debug/blobs/")
 
         device = "windows"
     
@@ -48,7 +48,12 @@ if __name__ == "__main__":
     # run all tests
     for file in os.listdir("integration_test"):
         if(file.endswith("_test.dart")):
-            ret_val = subprocess.run(f"flutter test -d {device} ./integration_test/{file}", shell=True)
+            command =  "flutter drive "
+            command += "--driver=test_driver/integration_test.dart "
+            command += "--target=integration_test/draw_screen_test.dart "
+            command += f"-d {device} "
+            command += "--profile "
+            ret_val = subprocess.run(command, shell=True)
             if(ret_val.returncode != 0):
                 print(f"Error in {file}, returned {ret_val}")
                 sys.exit(-1)
