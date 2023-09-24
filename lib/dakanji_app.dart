ea2b07539074aa851a545f4f5ee1b96ec4a2efc9
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-09-24 21:56:15 +0200

    separated on tutorial step

--- lib/dakanji_app.dart
+++ lib/dakanji_app.dart
@@ -1,4 +1,5 @@
 // Flutter imports:
+import 'package:da_kanji_mobile/application/tutorial/tutorial_on_step.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
@@ -60,49 +61,7 @@ class _DaKanjiAppState extends State<DaKanjiApp> {
                   globalOnboarding: true,
                   autoSizeTexts: true,
                   steps: GetIt.I<Tutorials>().getSteps(),
-                  onChanged: (int index) async {
-                    debugPrint("Tutorial step: $index");
-                    if(index == GetIt.I<Tutorials>().drawScreenTutorial.indexes!.last){
-                      debugPrint("DrawScreen tutorial done, saving...");
-                      GetIt.I<UserData>().showTutorialDrawing = false;
-                      await GetIt.I<UserData>().save();
-                    }
-                    else if(index == GetIt.I<Tutorials>().dictionaryScreenTutorial.indexes!.last){
-                      debugPrint("DictionaryScreen tutorial done, saving...");
-                      GetIt.I<UserData>().showTutorialDictionary = false;
-                      await GetIt.I<UserData>().save();
-                    }
-                    else if(index == GetIt.I<Tutorials>().textScreenTutorial.indexes!.last){
-                      debugPrint("TextScreen tutorial done, saving...");
-                      GetIt.I<UserData>().showTutorialText = false;
-                      await GetIt.I<UserData>().save();
-                    }
-                    else if(index == GetIt.I<Tutorials>().clipboardScreenTutorial.indexes!.last){
-                      debugPrint("Clipboard screen tutorial done, saving...");
-                      GetIt.I<UserData>().showTutorialClipboard = false;
-                      await GetIt.I<UserData>().save();
-                    }
-                    else if(index == GetIt.I<Tutorials>().kanjiTableScreenTutorial.indexes!.last){
-                      debugPrint("Kanji table screen tutorial done, saving...");
-                      GetIt.I<UserData>().showTutorialKanjiTable = false;
-                      await GetIt.I<UserData>().save();
-                    }
-                    else if(index == GetIt.I<Tutorials>().kanaTableScreenTutorial.indexes!.last){
-                      debugPrint("Kana table screen tutorial done, saving...");
-                      GetIt.I<UserData>().showTutorialKanaTable = false;
-                      await GetIt.I<UserData>().save();
-                    }
-                    else if(index == GetIt.I<Tutorials>().wordListsScreenTutorial.indexes!.last){
-                      debugPrint("Word lists screen tutorial done, saving...");
-                      GetIt.I<UserData>().showTutorialWordLists = false;
-                      await GetIt.I<UserData>().save();
-                    }
-                    else if(index == GetIt.I<Tutorials>().dojgScreenTutorial.indexes!.last){
-                      debugPrint("Dojg screen tutorial done, saving...");
-                      GetIt.I<UserData>().showTutorialDojg = false;
-                      await GetIt.I<UserData>().save();
-                    }
-                  },
+                  onChanged: onTutorialStep,
                   child: screen,
                 );
             },
