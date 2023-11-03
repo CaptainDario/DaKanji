f37f62523ef31f2cfb8a2df70406ea8174855f61
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-11-03 19:26:01 +0100

    added kanji animation settings texts

--- lib/locales_keys.dart
+++ lib/locales_keys.dart
@@ -286,8 +286,11 @@ abstract class  LocaleKeys {
   static const SettingsScreen_dict_disable_english_info = 'SettingsScreen.dict_disable_english_info';
   static const SettingsScreen_dict_kanaize = 'SettingsScreen.dict_kanaize';
   static const SettingsScreen_dict_kanaize_body = 'SettingsScreen.dict_kanaize_body';
+  static const SettingsScreen_dict_kanji_animation_strokes_per_second = 'SettingsScreen.dict_kanji_animation_strokes_per_second';
   static const SettingsScreen_dict_languages = 'SettingsScreen.dict_languages';
   static const SettingsScreen_dict_matrix_word_levels = 'SettingsScreen.dict_matrix_word_levels';
+  static const SettingsScreen_dict_play_kanji_animation_when_opened = 'SettingsScreen.dict_play_kanji_animation_when_opened';
+  static const SettingsScreen_dict_resume_animation_after_stop_scroll = 'SettingsScreen.dict_resume_animation_after_stop_scroll';
   static const SettingsScreen_dict_show_wikipedia_definitions = 'SettingsScreen.dict_show_wikipedia_definitions';
   static const SettingsScreen_dict_show_word_freq = 'SettingsScreen.dict_show_word_freq';
   static const SettingsScreen_dict_show_word_freq_body = 'SettingsScreen.dict_show_word_freq_body';
