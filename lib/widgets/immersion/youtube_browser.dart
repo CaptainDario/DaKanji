217e2131a15b24b2b9813753b13c15c79bb4df47
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-11-18 11:10:40 +0100

    added youtube

--- lib/widgets/immersion/youtube_browser.dart
+++ lib/widgets/immersion/youtube_browser.dart
@@ -1,3 +1,5 @@
+import 'package:da_kanji_mobile/widgets/immersion/youtube_fullscreen.dart';
+import 'package:da_kanji_mobile/widgets/widgets/da_kanji_loading_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
@@ -13,9 +15,19 @@ class YoutubeBrowser extends StatefulWidget {
 
 class _YoutubeBrowserState extends State<YoutubeBrowser> {
 
+  /// Webview params to create the webview controller
   late final PlatformWebViewControllerCreationParams params;
-
+  /// Webviewcontroller to manage the webview that shows youtube
   late final WebViewController _webViewController;
+  /// is youtube still loading
+  bool youtubeLoading = true;
+
+  String currentUrl = "";
+
+  String? currentVideoID;
+
+  RegExp videoIdRegex = RegExp(r"watch\?v=(.*)");
+
 
 
   @override
@@ -31,51 +43,78 @@ class _YoutubeBrowserState extends State<YoutubeBrowser> {
   }
 
   void init(){
+
+    // on iOS explicitly allow inline video playback
     if (WebViewPlatform.instance is WebKitWebViewPlatform) {
       params = WebKitWebViewControllerCreationParams(
         allowsInlineMediaPlayback: true,
-        
       );
-    } else {
+    }
+    else {
       params = const PlatformWebViewControllerCreationParams();
     }
 
     _webViewController = WebViewController.fromPlatformCreationParams(params)
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onPageFinished: (url) {
-          },
-        )
-      );
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageFinished: (url) async {
+          
+        },
+        onUrlChange: (change) async {
+          if(youtubeLoading) youtubeLoading = false;
 
-    _webViewController.loadRequest(Uri.parse("https://www.youtube.com/watch?v=mwKJfNYwvm8"))
-      .then((value) => setState(() {}));
+          currentUrl = await _webViewController.currentUrl() ?? "";
+          currentVideoID = videoIdRegex.firstMatch(currentUrl)?.group(1);
 
+          setState(() {});
+        },
+        //onNavigationRequest: (request) {
+          //if(!["www.youtube.com", "youtube.com",
+          //  "youtube.com", "youtube.com"].any((e) => request.url.startsWith(e))){
+          //  return NavigationDecision.prevent;
+          //}
+          //return NavigationDecision.navigate;
+        //},
+      ));
+
+    _webViewController
+      .loadRequest(Uri.parse("https://www.youtube.com/watch?v=mwKJfNYwvm8"));
   }
 
   @override
   void dispose() {
+    //_webViewController.loadRequest(Uri.parse('about:blank'));
     super.dispose();
   }
 
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        WebViewWidget(
-          controller: _webViewController,
-        ),
-        Positioned(
-          bottom: 16,
-          right: 16,
-          child: FloatingActionButton(
-            onPressed: () {},
-            child: const Icon(Icons.fullscreen),
+    return youtubeLoading
+      ? const DaKanjiLoadingIndicator()
+      : Stack(
+        children: [
+          WebViewWidget(
+            controller: _webViewController,
           ),
-        )
-      ],
-    );
+          if(currentVideoID != null)
+            Positioned(
+              bottom: 16,
+              right: 16,
+              child: FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(
+                    context, 
+                    MaterialPageRoute(
+                      builder: (BuildContext context) =>
+                        YoutubeFullscreen(currentVideoID!)
+                    )
+                  );
+                },
+                child: const Icon(Icons.fullscreen),
+              ),
+            )
+        ],
+      );
   }
 }
