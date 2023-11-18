7f30f9bdcd4cc31831111b331c564b55a4f2ff48
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-11-18 23:29:06 +0100

    setting to disable kana sound

---
+++ lib/domain/settings/settings_kana_table.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'settings_kana_table.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SettingsKanaTable _$SettingsKanaTableFromJson(Map<String, dynamic> json) =>
+    SettingsKanaTable()..playAudio = json['playAudio'] as bool? ?? true;
+
+Map<String, dynamic> _$SettingsKanaTableToJson(SettingsKanaTable instance) =>
+    <String, dynamic>{
+      'playAudio': instance.playAudio,
+    };
