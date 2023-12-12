d3dfa09cea50a74b9751d230fd436013bb8e1a29
CaptainDario @ MBP M1 <daapplab@gmail.com>
2023-12-12 19:38:06 +0100

    updated for material 3

--- lib/entities/theme/light_theme.dart
+++ lib/entities/theme/light_theme.dart
@@ -8,9 +8,18 @@ import 'package:da_kanji_mobile/globals.dart';
 final ThemeData lightTheme = ThemeData(
   
   brightness: Brightness.light,
+  colorSchemeSeed: Colors.white,
 
-  primarySwatch: createMaterialColor(g_Dakanji_green),
+  //primarySwatch: createMaterialColor(g_Dakanji_green),
+
+  appBarTheme: const AppBarTheme(
+    color: g_Dakanji_green,
+    surfaceTintColor: Colors.transparent,
+    scrolledUnderElevation: 0,
+    foregroundColor: Colors.white
+  ),
 
   highlightColor: g_Dakanji_red,
 
+  typography: Typography.material2018()
 );
