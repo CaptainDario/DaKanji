2ffd0d79299fa9621e2123ea410c84d399af17ee
CaptainDario @ MBP M1 <daapplab@gmail.com>
2024-01-15 19:06:50 +0100

    udpated actions

--- .github/workflows/create_dakanji_env.py
+++ .github/workflows/create_dakanji_env.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     with open("dakanji.env", mode="w+") as f:
 
         f.write(f"SENTRY_DSN={os.environ['SENTRY_DSN']}\n")
-        f.write(f"APTABASE_APP_KEY={os.environ['APTABASE_APP_KEY']}\n")
+        f.write(f"POSTHOG_API_KEY={os.environ['POSTHOG_API_KEY']}\n")
 
