bea6423e774fe44c1f2f637799024a7759e38a41
Dariyooo <daapplab@gmail.com>
2024-01-20 12:14:52 +0100

    updated

--- .github/workflows/create_dakanji_env.py
+++ .github/workflows/create_dakanji_env.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with open("dakanji.env", mode="w+") as f:
 
         f.write(f"SENTRY_DSN={os.environ['SENTRY_DSN']}\n")
-        f.write(f"POSTHOG_API_KEY_REL={os.environ['POSTHOG_API_KEY_REL']}\n")
+        f.write(f"POSTHOG_API_KEY_REL={os.environ['POSTHOG_API_KEY_DEV']}\n")
         f.write(f"POSTHOG_API_KEY_DEV={os.environ['POSTHOG_API_KEY_DEV']}\n")
 
