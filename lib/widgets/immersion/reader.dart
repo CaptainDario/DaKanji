2d626e68f4ad0da61305d0eb9a7cc86912ab281e
CaptainDario @ MBP M1 <daapplab@gmail.com>
2024-04-22 21:54:40 +0900

    misc

---
+++ lib/widgets/immersion/reader.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class Reader extends StatefulWidget {
+  const Reader({super.key});
+
+  @override
+  State<Reader> createState() => _ReaderState();
+}
+
+class _ReaderState extends State<Reader> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
