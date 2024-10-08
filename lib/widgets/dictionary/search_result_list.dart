488b7955497a706809fe8ea9728048e7587fa6af
CaptainDario @ MBP M1 <daapplab@gmail.com>
2024-10-08 21:44:19 +0900

    improved when the slide in animation happens

--- lib/widgets/dictionary/search_result_list.dart
+++ lib/widgets/dictionary/search_result_list.dart
@@ -53,7 +53,7 @@ class SearchResultList extends StatefulWidget {
 class _SearchResultListState extends State<SearchResultList> {
 
   /// Key to either only slide in search results once, or multiple times
-  Key slideInAnimationKey = Key(DateTime.now().toIso8601String());
+  Key? slideInAnimationKey;
   /// List of focus nodes that each corresponds to on search result entry 
   List searchResultsFocusses = [];
   /// Controller that can be used to hadle this instance
