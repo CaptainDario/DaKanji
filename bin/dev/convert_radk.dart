854b9f44c767c3325379aa2826c6418d8548b681
CaptainDario @ MBP M1 <daapplab@gmail.com>
2024-11-23 22:50:07 +0900

    added: radk conversion

---
+++ bin/dev/convert_radk.dart
@@ -0,0 +1,11 @@
+import 'package:dakanji_db/conversion/krad.dart';
+
+
+
+void main() async {
+
+  // File path
+  final filePath = 'input_files/kradzip/radkfilex';
+  convertRadkToMap(filePath);
+
+}
