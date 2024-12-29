589321975fed0fca6422367cb2c131f7cd8b0c64
CaptainDario @ MBP M1 <daapplab@gmail.com>
2024-12-29 23:12:25 +0100

    join path

--- lib/init.dart
+++ lib/init.dart
@@ -188,7 +188,7 @@ Future<void> initDocumentsServices(BuildContext context) async {
   // Mecab
   GetIt.I.registerSingleton<Mecab>(Mecab());
 
-  await GetIt.I<Mecab>().init("$supportDirectory/assets/mecab_dict/", true);
+  await GetIt.I<Mecab>().init(p.joinAll([supportDirectory, "assets", "mecab_dict"]), true);
 
   g_documentsServicesInitialized = true;
 }
