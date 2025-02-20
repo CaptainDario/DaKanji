cf46cbc445fda84c9098ad0f8a5bc3e97bb56bc9
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-20 12:02:24 +0900

    moved mecab vars to separate file

---
+++ lib/application/japanese_text_processing/mecab_data.dart
@@ -0,0 +1,90 @@
+/// verb
+String verb = "動詞";
+/// i adjective
+String iAdjective = "形容詞";
+/// na adjective
+String naAdjective = "形状詞";
+/// noun
+String noun = "名詞";
+// PoS for particles
+String particle = "助詞";
+
+List<String> mecabPosDeconjugate = [
+  verb, iAdjective, naAdjective,
+  //noun
+];
+
+/// Inflection dependent word
+String inflectionDependentWord = "助動詞";
+/// Conjunction particle
+String conjunctionParticle = "接続助詞";
+/// Words that cannot standalone
+String nonIndependent = "非自立可能";
+/// Suffix
+String suffix = "接尾辞";
+/// The verb / adjective that starts this word
+String startPos = "一般";
+
+/// A list of mecab pos combinations that indicate a word start
+List<List<String>> mecabPosWordStart = [
+  // verb
+  [verb],
+  // i-adj
+  [iAdjective, startPos],
+  [iAdjective, nonIndependent],
+  // na-adj
+  [naAdjective, startPos],
+  [naAdjective, nonIndependent],
+  // nouns
+  [noun]
+];
+
+List<List<String>> wordContinuationPOS = [
+  // general
+  [inflectionDependentWord],
+  [particle, conjunctionParticle],
+  [suffix],
+  // verb
+  [verb, nonIndependent],
+  // i-adj
+  [iAdjective, nonIndependent],
+  // na-adj
+  [naAdjective, nonIndependent],
+];
+
+/// Compares `a` to `b`, if the strings are the same.
+/// If `b` is shorter than `a`, the comparison only checks
+/// the first elements
+bool compareMecabOut(List<String> a, List<String?> b,){
+
+  bool isSame = true;
+
+  if(a[0] != b[0]){
+    isSame = false;
+  }
+  else if(b.length > 1 && a[1] != b[1]){
+    isSame = false;
+  }
+  else if(b.length > 2 && a[2] != b[2]){
+    isSame = false;
+  }
+  else if(b.length > 3 && a[3] != b[3]){
+    isSame = false;
+  }
+
+  return isSame;
+
+}
+
+/// Compares one mecab output to a list of mecab outputs.
+/// If any of them are equal returns true
+bool compareMecabOuts(List<String> a, List<List<String>> mecabOuts){
+
+  for (var mecabOut in mecabOuts) {
+    
+    if(compareMecabOut(a, mecabOut)) return true;
+
+  }
+
+  return false;
+}
