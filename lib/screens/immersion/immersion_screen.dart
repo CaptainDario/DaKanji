6d16722d8c1f73c3a02d7001850e3e64683135c9
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-05-05 21:54:16 +0200

    auto show video player

--- lib/screens/immersion/immersion_screen.dart
+++ lib/screens/immersion/immersion_screen.dart
@@ -1,10 +1,13 @@
 // Flutter imports:
+import 'package:da_kanji_mobile/widgets/video/video_player.dart';
+import 'package:da_kanji_mobile/widgets/video/video_widget.dart';
 import 'package:flutter/material.dart';
 
 // Project imports:
 import 'package:da_kanji_mobile/entities/screens.dart';
 import 'package:da_kanji_mobile/widgets/drawer/drawer.dart';
 import 'package:da_kanji_mobile/widgets/reading/reading_widget.dart';
+import 'package:universal_io/io.dart';
 
 class ImmersionScreen extends StatefulWidget {
 
@@ -28,15 +31,27 @@ class ImmersionScreen extends StatefulWidget {
 
 class _ImmersionScreenState extends State<ImmersionScreen> {
 
+  String videoPath = "/Users/darioklepoch/dev/DaKanji/testing_files/aot_s4_e1";
+
+  late File videoFile;
+  late File subsFile;
+
+
+  @override
+  void initState() {
+    videoFile = File("$videoPath.mp4");
+    subsFile = File("$videoPath.srt");
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return DaKanjiDrawer(
       drawerClosed: !widget.openedByDrawer,
       currentScreen: Screens.immersion,
-      child: ReadingWidget(
-        widget.openedByDrawer,
-        widget.includeTutorial
-      )
+      child:
+        VideoPlayer(videoFile, subsFile)
+      //ReadingWidget(widget.openedByDrawer, widget.includeTutorial)
     );
   }
 
