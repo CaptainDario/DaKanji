580478fa46627e5fbf5b0a22cca258435d0f3205
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-07-24 10:16:33 +0200

    fts 5 queries

--- lib/database/example/example_dao.dart
+++ lib/database/example/example_dao.dart
@@ -1,5 +1,8 @@
 // Package imports:
+import "package:dakanji_db/database/example/example_entry.dart";
+import "package:dakanji_db/database/example/example_entry_translation.dart";
 import "package:dakanji_db/database/example/example_tables.dart";
+import "package:dakanji_db/database/example/example_view.dart";
 import "package:drift/drift.dart";
 
 // Project imports:
@@ -10,12 +13,84 @@ part 'example_dao.g.dart';
 
 
 // Dao class that contains all queries related to the `ExampleTable`
-@DriftAccessor(tables: [
-  ExampleTable, ExampleTranslationTable
-])
+@DriftAccessor(
+  tables: [
+    ExampleTable, ExampleTranslationTable
+  ],
+  views: [
+    ExampleView
+  ],
+  include: {
+    'example_fts5.drift'
+  }
+)
 class ExampleDao extends DatabaseAccessor<DaKanjiDB> with _$ExampleDaoMixin {
   
   ExampleDao(super.db);
+
+  Future<List<ExampleEntry>> searchExamples(String query) async {
+
+    List<ExampleTableData> results = await customSelect(
+      'SELECT T.* FROM example_table AS T '
+      'INNER JOIN example_fts AS F ON F.rowid = T.id '
+      'WHERE example_fts MATCH :query',
+      // The variables map will be used to expand the :query parameter.
+      variables: [Variable.withString(query)],
+      // We need to tell drift how to parse the result set.
+      readsFrom: {exampleTable, exampleFts},
+    ).map((row) => exampleTable.map(row.data)).get();
+    List<int> ids = results.map((e) => e.id).toList();
+
+    (select(exampleTable)
+    ..join([
+      innerJoin(exampleFts, exampleFts.rowId.equalsExp(exampleTable.id)),
+    ])
+    ..where((_) => exampleFts.match(query)));
+
+    final _query = (selectOnly(exampleTable)
+      .join([
+        // join translations via its relation table
+        innerJoin(
+          exampleTranslationRelationsTable,
+          exampleTable.id.equalsExp(exampleTranslationRelationsTable.exampleId)
+        ),
+        innerJoin(
+          exampleTranslationTable,
+          exampleTranslationRelationsTable.translationId.equalsExp(exampleTranslationTable.id)
+        )
+      ])
+    )
+    
+    ..groupBy([exampleTable.id]) 
+    ..addColumns([
+      exampleTable.exampleSentence,
+      
+      exampleTranslationTable.exampleTranslation.groupConcat(distinct: true)
+      
+    ]);
+    final joined = await _query.get();
+
+    print(joined);
+
+    return results.map((e) => ExampleEntry(
+      example: e.exampleSentence,
+      tokenizedExample: e.exampleSentenceTokenized,
+      translations: []
+    )).toList();
+
+  }
+
+  Future getExampleById(int id) async {
+
+    int m = await maxExampleId();
+    var exampleQuery = (select(exampleTable)
+      ..where((tbl) => tbl.id.equals(m)));
+
+    // get the actual text
+    var example = await exampleQuery.getSingle();
+    return example.exampleSentenceTokenized;
+
+  }
 
   // ---------------------------------------------------------------------------
   /// Get the maximum id of the [ExampleTable]
--- lib/database/example/example_dao.g.dart
+++ lib/database/example/example_dao.g.dart
@@ -5,6 +5,7 @@ part of 'example_dao.dart';
 // ignore_for_file: type=lint
 mixin _$ExampleDaoMixin on DatabaseAccessor<DaKanjiDB> {
   $ExampleTableTable get exampleTable => attachedDatabase.exampleTable;
+  ExampleFts get exampleFts => attachedDatabase.exampleFts;
   $LanguageCodeTableTable get languageCodeTable =>
       attachedDatabase.languageCodeTable;
   $ExampleTranslationTableTable get exampleTranslationTable =>
