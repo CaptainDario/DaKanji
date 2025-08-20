8f2c654cce0781e2784482e1ac9151327842abd4
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-08-20 14:45:50 +0200

    bumped sentry

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -19,6 +19,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
   lite_rt_for_flutter_libs_linux
 )
 
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -21,6 +21,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
   lite_rt_for_flutter_libs_windows
 )
 
