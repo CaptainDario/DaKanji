d40d91b04454a1fccffb9bdf011acf7452aa4ea0
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-08-29 21:48:24 +0200

    do not use mecab for sentence finding

--- lib/parsing/japanese_processing/sentence_finding.dart
+++ lib/parsing/japanese_processing/sentence_finding.dart
@@ -1,5 +1,3 @@
-import 'package:mecab_for_dart/mecab_dart.dart';
-
 /// matches full and half width punctuations
 const String punctuations = "。|？|！|\\.|\\!|\\?";
 /// matches japanese ending parantheses
@@ -12,25 +10,6 @@ RegExp sentenceRegex = RegExp(
   multiLine: true
 );
 
-
-// Finds all sentences in the given `text` using mecab for estimation
-List<String> findSentencesMecab(String text, Mecab mecab){
-
-  List<TokenNode> tokens = mecab.parse(text);
-  List<String> sentences = [];
-  StringBuffer currentSentence = StringBuffer();
-
-  for (TokenNode token in tokens) {
-    if (token.surface == "EOS") {
-      sentences.add(currentSentence.toString());
-      currentSentence.clear();
-    } else {
-      currentSentence.write(token.surface);
-    }
-  }
-
-  return sentences;
-}
 
 // Finds all sentences in the given `text` using a RegExp for estimation
 List<String> findSentencesRegexp(String text){
