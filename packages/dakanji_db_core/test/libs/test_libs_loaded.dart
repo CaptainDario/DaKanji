fa9e532f854ca654f7f571bca71f699e2571cb33
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-09-04 22:33:06 +0200

    test to check if all libs load correctly

---
+++ packages/dakanji_db_core/test/libs/test_libs_loaded.dart
@@ -0,0 +1,39 @@
+import 'package:test/test.dart';
+import 'package:sqlite3/native_assets.dart';
+import 'package:dakanji_db_core/extensions/sqlite_vec_extension.dart';
+import 'package:dakanji_db_core/extensions/sqlite_spellfix_extension.dart';
+import 'package:dakanji_db_core/extensions/sqlite_crsqlite_extension.dart';
+
+
+void main() async {
+
+  sqlite3Native.loadSqliteVecExtension();
+  sqlite3Native.loadSqliteSpellfixExtension();
+  sqlite3Native.loadSqliteCrsqliteExtension();
+
+  final db = sqlite3Native.openInMemory();
+
+  test('Testing FTS5 loaded', () async {
+    db.execute('CREATE VIRTUAL TABLE email USING fts5(sender, title, body);');
+  });
+
+  test('Testing DBSTAT loaded', () async {
+    db.execute('SELECT * FROM dbstat;');
+  });
+
+  test('Testing SPELLFIX loaded', () async {
+    db.execute('CREATE VIRTUAL TABLE demo USING spellfix1;');
+  });
+
+  test('Testing SQLite-vec loaded', () async {
+    db.execute('create virtual table vec_examples using vec0(sample_embedding float[8]);');
+  });
+
+  test('Testing CR-SQLite loaded', () async {
+    db.execute('''
+      create table foo (a primary key not null, b);
+      select crsql_as_crr('foo');
+    ''');
+  });
+
+}
