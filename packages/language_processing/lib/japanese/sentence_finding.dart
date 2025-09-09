2f575e12cae4220bcc52e5cce94507eef9d27e1e
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-09-09 17:26:49 +0200

    moved regex things to string operations

--- packages/language_processing/lib/japanese/sentence_finding.dart
+++ packages/language_processing/lib/japanese/sentence_finding.dart
@@ -1,14 +1,4 @@
-/// matches full and half width punctuations
-const String punctuations = "。|？|！|\\.|\\!|\\?";
-/// matches japanese ending parantheses
-const String japaneseParantheses = "』|」";
-/// matches any whitespace
-const String anyWhiteSpace = "\\s|　";
-/// Regex that matches a sentence
-RegExp sentenceRegex = RegExp(
-  "(?:[^$anyWhiteSpace])+?(?:(?!($punctuations)$japaneseParantheses)$punctuations|\\n|\$)",
-  multiLine: true
-);
+import 'japanese_string_operations.dart';
 
 // Finds all sentences in the given `text` using a RegExp for estimation
 List<String> findSentencesRegexp(String text){
