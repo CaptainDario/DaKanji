94dc05cedc18df48826fcfebd15e42f604b107c0
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-09-10 19:34:59 +0200

    initial search draft

--- packages/dakanji_db_core/lib/database/dakanji_db_dao.dart
+++ packages/dakanji_db_core/lib/database/dakanji_db_dao.dart
@@ -1,5 +1,8 @@
 // Package imports:
+import "package:dakanji_db_core/database/term/term_bank_v3_entry.dart";
+import "package:dakanji_db_core/queries/term_search.dart";
 import "package:drift/drift.dart";
+import "package:language_processing/iso/iso_table.dart";
 
 // Project imports:
 import "dakanji_db.dart";
@@ -14,8 +17,36 @@ class DaKanjiDBDao extends DatabaseAccessor<DaKanjiDB> with _$DaKanjiDBDaoMixin 
   // of this object.
   DaKanjiDBDao(super.db);
 
-  void test(){
+  Future<List<TermBankV3Entry>> searchTerm(
+    String term,
+    List<Iso639_1> languages,
+    List<String> tags,
+    bool convertRomajiToHiragana,
+    {
+      int limit=-1,
+      int offset=0
+    }
+  ) async {
 
+    final (:hiraganaTerm, term: preprocessedTerm) =
+      preprocessInput(term, convertRomajiToHiragana);
+
+    
+
+    // check laguages are set and parse 
+    assert (languages.isNotEmpty);
+    List<String> langs = languages.map((e) => e.name).toList();
+
+    List<TermBankV3SearchViewData> t = 
+      await db.term_bank_v3_search(preprocessedTerm, limit, offset).get();
+
+    List<TermBankV3Entry> results = t
+      .map((e) => TermBankV3Entry.fromTermBankV3SearchViewData(e))
+      .toList();
+
+    return results;
+
+    
   }
   
 }
