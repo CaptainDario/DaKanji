eb9ecac8d7c63d647306746d37a713c65e384475
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-10-06 09:50:23 +0900

    added get by title

--- packages/dakanji_db_core/lib/database/index/index_dao.dart
+++ packages/dakanji_db_core/lib/database/index/index_dao.dart
@@ -17,6 +17,18 @@ class IndexDao extends DatabaseAccessor<DaKanjiDB> with _$IndexDaoMixin {
   // instance of this object.
   IndexDao(super.db);
 
+  /// returns the entry with `title` if found otherwise `null`
+  Future<IndexTableData?> getByTitle(String title) async {
+
+    final query = select(db.indexTable)..where((tbl) => tbl.title.equals(title));
+
+    // Fetch the first result that matches the condition
+    final result = query.getSingleOrNull();
+
+    return result;
+
+  }
+
   /// returns the entry with `id` if found otherwise `null`
   Future<IndexTableData?> getById(int id) async {
 
