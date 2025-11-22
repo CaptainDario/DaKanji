fe6491bf96c1ce92dd05cebd6aa60a80c22f9b75
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-11-22 19:15:45 +0900

    added custom icons to icon dataclass

--- apps/dakanji/lib/core/icons/da_kanji_icons.dart
+++ apps/dakanji/lib/core/icons/da_kanji_icons.dart
@@ -1,9 +1,12 @@
 // Flutter imports:
+import 'package:da_kanji_mobile/core/icons/da_kanji_icons_icons.dart';
 import 'package:flutter/material.dart';
 
 class DaKanjiIcons {
   
   static const IconData about = Icons.info_outline;
+  static const IconData anki = DaKanjiCustomIcons.anki;
+  static const IconData dojg = DaKanjiCustomIcons.dojg;
   static const IconData drawing = Icons.brush;
   static const IconData dictionary = Icons.book;
   static const IconData reading = Icons.auto_stories;
@@ -14,11 +17,15 @@ class DaKanjiIcons {
   static const IconData text = Icons.text_snippet;
   static const IconData clipboard = Icons.copy_rounded;
   static const IconData kuzushiji = IconData(0x5d29, fontFamily: "kouzan");
+  static const IconData kanjiTrainer = DaKanjiCustomIcons.kanji_trainer;
+  static const IconData kanaTrainer = DaKanjiCustomIcons.kana_trainer;
+  static const IconData kanjiTable = DaKanjiCustomIcons.kanji_table;
+  static const IconData kanaTable = DaKanjiCustomIcons.kana_table;
   static const IconData wordLists = Icons.list_outlined;
   static const IconData settings = Icons.settings_applications_outlined;
   static const IconData manual = Icons.help;
   static const IconData misc = Icons.miscellaneous_services_outlined;
   static const IconData deepLinks = Icons.link;
   static const IconData feedback = Icons.feedback_outlined;
-
+  static const IconData timer = Icons.timer;
 }
