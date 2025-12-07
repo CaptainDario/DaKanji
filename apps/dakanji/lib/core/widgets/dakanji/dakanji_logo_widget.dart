dc334e1429d5cfb9bcc2961d058e1edb75588a35
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-12-07 09:13:48 +0900

    param for row / column placement

--- apps/dakanji/lib/core/widgets/dakanji/dakanji_logo_widget.dart
+++ apps/dakanji/lib/core/widgets/dakanji/dakanji_logo_widget.dart
@@ -5,35 +5,44 @@ import 'package:flutter/material.dart';
 
 class DakanjiLogoWidget extends StatelessWidget {
 
+  /// The size multiplier for the logo
   final double size;
-
+  /// If true, show the version text below the logo
   final bool showVersion;
+  /// If true, use a column layout instead of a row
+  final bool useColumn;
 
   const DakanjiLogoWidget(
     {
       this.size = 1,
       this.showVersion = false,
+      this.useColumn = false,
       super.key
     }
   );
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Row(
-        //crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(
-            "assets/images/dakanji/icon.png",
-            height: 36*size,
-          ),
-          SizedBox(width: 8),
-          DaKanjiLogoText(
-            fontSize: 40*size,
-            showVersion: showVersion,
-          ),
-        ],
+
+    final children = [
+      Image.asset(
+        "assets/images/dakanji/icon.png",
+        height: (!useColumn ? 36 : 72)*size,
       ),
-    );
+      SizedBox(width: 8),
+      DaKanjiLogoText(
+        fontSize: 40*size,
+        showVersion: showVersion,
+      ),
+    ];
+
+    return !useColumn  
+      ? Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: children,
+      )
+      : Column(
+        children: children, 
+      );
   }
 }
