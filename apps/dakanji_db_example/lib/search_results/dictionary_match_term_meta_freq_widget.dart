1bde73c3276f85758258ae08bcfdf06c661a59d0
CaptainDario @ MBP M1 <daapplab@gmail.com>
2026-01-03 22:17:16 +0100

    refactor tags

--- apps/dakanji_db_example/lib/search_results/dictionary_match_term_meta_freq_widget.dart
+++ apps/dakanji_db_example/lib/search_results/dictionary_match_term_meta_freq_widget.dart
@@ -10,48 +10,73 @@ class DictionaryMatchTermMetaFreqWidget extends StatelessWidget {
 
   const DictionaryMatchTermMetaFreqWidget(this.freqTermMetaEntries, {super.key});
 
+  List<({List<String> texts, List<Color?> textColors})> prepareTags(
+    Iterable<MapEntry<int, List<TermMetaBankV3Entry>>> entries,
+    bool containsMultipleGroupedEntries
+  ) {
+
+    List<({List<String> texts, List<Color?> textColors})> ret = [];
+
+    for (final MapEntry(key: index, value: entries) in entries){
+
+      List<String> texts = []; List<Color?> textColors = [];
+      String currentText = ""; bool firstNoReading = true;
+      String lastGroupingTerm = "";
+
+      for (var i = 0; i < entries.length; i++) {
+        final e = entries[i];
+
+        if(
+          containsMultipleGroupedEntries && // this contains multiple grouped entries
+          entries[i].term != lastGroupingTerm // and this is a new term
+        ){
+          lastGroupingTerm = entries[i].term;
+          if(i != 0) {
+            texts.add(currentText);
+            textColors.add(null);
+            currentText = "";
+          }
+          texts.add(
+            "${entries[i].term}${entries[i].reading == null ? "${entries[i].reading}" : ""} "
+          );
+          textColors.add(Colors.grey);
+        }
+
+        if(firstNoReading && i != 0 && e.reading == null) {
+          currentText += " ◆ ";
+          firstNoReading = false;
+        }
+        currentText += "${e.frequencyDisplayValue ?? e.frequency}";
+        if(i != entries.length -1 &&
+          entries[i+1].reading != null &&
+          (entries[i+1].term == lastGroupingTerm || lastGroupingTerm == "")) {
+          currentText += ", ";
+        }
+      }
+      ret.add((texts: texts, textColors: textColors));
+    }
+
+    return ret;
+  }
+
   @override
   Widget build(BuildContext context) {
 
     List<String> allTerms = freqTermMetaEntries.values.expand((e) => e.map((e) => e.term)).toSet().toList();
     bool containsMultipleGroupedEntries = allTerms.length > 1;
+    final tags = prepareTags(
+      freqTermMetaEntries.entries,
+      containsMultipleGroupedEntries
+    );
 
     return Wrap(
       spacing: 4,
       runSpacing: 4,
       children: [
-        for (final MapEntry(key: index, value: entries) in freqTermMetaEntries.entries)
+        for (final (i, MapEntry(key: index, value: entries)) in freqTermMetaEntries.entries.indexed)
           DictionaryMatchTag(
-            text: () {
-              String text = ""; bool firstNoReading = true;
-              String lastGroupingTerm = "";
-
-              for (var i = 0; i < entries.length; i++) {
-                final e = entries[i];
-
-                if(
-                  containsMultipleGroupedEntries && // this contains multiple grouped entries
-                  entries[i].term != lastGroupingTerm // and this is a new term
-                ){
-                  lastGroupingTerm = entries[i].term;
-                  if(i != 0) text += " | ";
-                  text += "$lastGroupingTerm: ";
-                }
-
-                if(firstNoReading && i != 0 && e.reading == null) {
-                  text += " ◆ ";
-                  firstNoReading = false;
-                }
-                text += "${e.frequencyDisplayValue ?? e.frequency}";
-                if(i != entries.length -1 &&
-                  entries[i+1].reading != null &&
-                  (entries[i+1].term == lastGroupingTerm || lastGroupingTerm == "")) {
-                  text += ", ";
-                }
-              }
-
-              return text;
-            } (),
+            texts: tags[i].texts,
+            textColors: tags[i].textColors,
             leadingText: entries.first.indexEntry.title.toString(),
           )
       ]
