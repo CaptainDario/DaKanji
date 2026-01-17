e646400515fa04129ec7bcf631d87d05a8f2d359
CaptainDario @ MBP M1 <daapplab@gmail.com>
2026-01-17 16:46:13 +0100

    grouping setting widget

---
+++ packages/dakanji_db_ui/lib/widgets/settings/dakanji_db_settings_grouping_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class DakanjiDbSettingsGroupingWidget extends StatefulWidget {
+  const DakanjiDbSettingsGroupingWidget({super.key});
+
+  @override
+  State<DakanjiDbSettingsGroupingWidget> createState() => _DakanjiDbSettingsGroupingWidgetState();
+}
+
+class _DakanjiDbSettingsGroupingWidgetState extends State<DakanjiDbSettingsGroupingWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      
+    );
+  }
+}
