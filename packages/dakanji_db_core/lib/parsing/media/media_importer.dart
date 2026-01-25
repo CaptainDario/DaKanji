a3935531babad8d370466d6c79f6d8e07049b9cf
CaptainDario @ MBP M1 <daapplab@gmail.com>
2026-01-25 17:00:22 +0100

    normalized file names to counter os based differences

--- packages/dakanji_db_core/lib/parsing/media/media_importer.dart
+++ packages/dakanji_db_core/lib/parsing/media/media_importer.dart
@@ -1,6 +1,7 @@
 import 'package:dakanji_db_core/database/dakanji_db.dart';
 import 'package:drift/drift.dart';
 import 'package:path/path.dart' as p;
+import 'package:unorm_dart/unorm_dart.dart' as unorm;
 
 
 
@@ -8,14 +9,19 @@ Future<int> importMediaFile(
   String filePath, Uint8List mediaContent, int indexId, DaKanjiDB db, int? insertId
 ) async {
 
+  // get raw paths
   String name = p.basename(filePath);
   String path = p.dirname(filePath);
   
+  // Normalize path and name to NFC
+  String cleanName = unorm.nfc(name); 
+  String cleanPath = unorm.nfc(path);
+
   return await db.into(db.mediaTable).insert(MediaTableCompanion(
     id: insertId != null ? Value(insertId) : Value.absent(),
     indexId: Value(indexId),
-    path: Value(path),
-    name: Value(name),
+    path: Value(cleanPath),
+    name: Value(cleanName),
     data: Value(mediaContent),
   ));
 
