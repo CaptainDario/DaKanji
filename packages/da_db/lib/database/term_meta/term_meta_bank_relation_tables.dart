1748a2510f60df0db93c02bb3c2203800eee0182
CaptainDario @ MBP M1 <daapplab@gmail.com>
2026-02-28 09:51:04 +0100

    junction tables should use union keys

--- packages/da_db/lib/database/term_meta/term_meta_bank_relation_tables.dart
+++ packages/da_db/lib/database/term_meta/term_meta_bank_relation_tables.dart
@@ -5,13 +5,16 @@ import 'package:drift/drift.dart';
 import '/database/term_meta/term_meta_bank_v3_tables.dart';
 
 /// Relationship table between TermMetaBank and PitchTable
-@TableIndex(name: 'TermMetaBankV3_X_PitchTable_pitchIdIndex', columns: {#pitchId})
 @TableIndex(name: 'TermMetaBankV3_X_PitchTable_termMetaIdIndex', columns: {#termMetaId})
 // ignore: camel_case_types
 class TermMetaBankV3_X_PitchTable extends Table {
 
-  /// id of this relation
-  IntColumn get id => integer().autoIncrement()();
+  @override
+  bool get withoutRowId => true;
+
+  @override
+  Set<Column> get primaryKey => {pitchId, termMetaId};
+
   /// the id of the associated devoice
   IntColumn get pitchId => integer().references(TermMetaBankV3PitchTable, #id)();
   /// the id of the associated term meta
@@ -21,13 +24,16 @@ class TermMetaBankV3_X_PitchTable extends Table {
 }
 
 /// Relationship table between TermMetaBank and IpaTable
-@TableIndex(name: 'TermMetaBankV3_X_IpaTable_ipaIdIndex', columns: {#ipaId})
 @TableIndex(name: 'TermMetaBankV3_X_IpaTable_termMetaIdIndex', columns: {#termMetaId})
 // ignore: camel_case_types
 class TermMetaBankV3_X_IpaTable extends Table {
 
-  /// id of this relation
-  IntColumn get id => integer().autoIncrement()();
+  @override
+  bool get withoutRowId => true;
+
+  @override
+  Set<Column> get primaryKey => {ipaId, termMetaId};
+
   /// the id of the associated ipa entry
   IntColumn get ipaId => integer()
     .references(TermMetaBankV3IpaTable, #id)();
@@ -38,13 +44,16 @@ class TermMetaBankV3_X_IpaTable extends Table {
 }
 
 /// Relationship table between Ipa transcription and its tags
-@TableIndex(name: 'TermMetaBankV3IpaTable_X_TagBankV3Table_ipaIdIndex', columns: {#ipaId})
 @TableIndex(name: 'TermMetaBankV3IpaTable_X_TagBankV3Table_tagIdIndex', columns: {#tagId})
 // ignore: camel_case_types
 class TermMetaBankV3IpaTable_X_TagBankV3Table extends Table {
 
-  /// id of this relation
-  IntColumn get id => integer().autoIncrement()();
+  @override
+  bool get withoutRowId => true;
+
+  @override
+  Set<Column> get primaryKey => {ipaId, tagId};
+
   /// the id of the associated ipa entry
   IntColumn get ipaId => integer()
     .references(TermMetaBankV3IpaTable, #id, onDelete: KeyAction.cascade)();
@@ -55,13 +64,16 @@ class TermMetaBankV3IpaTable_X_TagBankV3Table extends Table {
 }
 
 /// Relationship table between pitch and its tags
-@TableIndex(name: 'TermMetaBankV3PitchTable_X_TagBankV3Table_pitchIdIndex', columns: {#pitchId})
 @TableIndex(name: 'TermMetaBankV3PitchTable_X_TagBankV3Table_tagIdIndex', columns: {#tagId})
 // ignore: camel_case_types
 class TermMetaBankV3PitchTable_X_TagBankV3Table extends Table {
 
-  /// id of this relation
-  IntColumn get id => integer().autoIncrement()();
+  @override
+  bool get withoutRowId => true;
+
+  @override
+  Set<Column> get primaryKey => {pitchId, tagId};
+
   /// the id of the associated pitch entry
   IntColumn get pitchId => integer()
     .references(TermMetaBankV3PitchTable, #id, onDelete: KeyAction.cascade)();
