671bcd18d81e4b153542b0cfc741bee98108ef4a
CaptainDario @ MBP M1 <daapplab@gmail.com>
2026-03-01 22:40:59 +0100

    updated audio list parsing test

--- packages/da_db/test/audio_source_list/audio_list_test.dart
+++ packages/da_db/test/audio_source_list/audio_list_test.dart
@@ -1,25 +1,19 @@
 
 import 'package:da_db/database/da_db.dart';
-import 'package:da_db/parsing/yomitan/in_memory_cache/audio_source_list/audio_source_list_parser.dart';
 import 'package:da_db_shared/paths.dart';
 import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
-import 'package:universal_io/io.dart';
 
-import '../dictionary_test_variables.dart';
+import '../test_utils/setup_fresh_db.dart';
 import 'audio_list_test_cases.dart';
 
 void main() async {
   
   // create the testing database (delete any existing database)
-  DaDb db = DaDb(
-    dbPath: daDbTestPath, inMemory: true, languageProcessor: await japaneseProcessor);
-  db.clearDB();
-
+  DaDb db = await setupFreshDb(p.join(daDbDataFilesPath, 'example_audio_sources'));
+  
   // convert the test files
   Stopwatch s = Stopwatch()..start();
-  await parseAudioFile(File(p.join(devExampleAudioListPath, "audio_list.json")), db, 1);
-  //await parseDictionaryFolder(Directory(devExampleAudioPath), db, true);
   print("Conversion took ${s.elapsedMilliseconds} ms");
   
   test('Test importing samples', () async {
--- packages/da_db/test/audio_source_list/audio_list_test_cases.dart
+++ packages/da_db/test/audio_source_list/audio_list_test_cases.dart
@@ -6,7 +6,7 @@ List<AudioSourceListEntry> audioListTestCases = [
     uri: "https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?kanji={{KANJI}}&kana={{KANA}}",
   ),
   AudioSourceListEntry(
-    name: "JapanesePod101",
-    uri: "https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?kanji={{KANJI}}&kana={{KANA}}",
+    name: "JapanesePod102",
+    uri: "https://assets.languagepod102.com/dictionary/japanese/audiomp3.php?kanji={{KANJI}}&kana={{KANA}}",
   ),
 ];
